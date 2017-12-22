7a06fee3daacccd6496a891d51055f8d0504aaa2
Ryan Coombes <ryan.coombes1@ntlworld.com>
2017-12-22 09:45:29 +0000

    Cleaned up code

--- CataclysmRaids/BlackwingDescent.lua
+++ CataclysmRaids/BlackwingDescent.lua
@@ -29,6 +29,7 @@ local aberrationsList = {}
 local aberrationsCounter = 0
 local aberrationsCounterKilled = 0
 local timerStarted = false
+local startTimer = false
 
 ------------------------------------------------------
 ---- Chimaeron
@@ -45,32 +46,27 @@ local step3Complete = false
 local step4Complete = false
 
 function core.BlackwingDescent:Magmaw()
-	core:displayAchievementsToTrackCurrent(5306)
-
-	if core.type == "SPELL_AURA_APPLIED" and core.spellId == 78941 and core.achievementFailed == false then
-		SendChatMessage("[WIP] "  .. GetAchievementLink(5306) .. " FAILED! by (" .. core.destName .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-		core.achievementFailed = true
+	if core.type == "SPELL_AURA_APPLIED" and core.spellId == 78941 then
+		core:getAchievementFailedWithMessageAfter("by (" .. core.destName .. ")")
 	end
 end
 
 function core.BlackwingDescent:OminitronDefenseSystem()
-	core:displayAchievementsToTrackCurrent(5307)
-
 	--Arcane Annihilator
 	if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 79710 and arcaneAnnihilatorFailed == false then
-		SendChatMessage("[WIP] " .. core.spellName .. " part of " .. GetAchievementLink(5307) .. " FAILED! by (" .. core.destName .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+		core:sendMessage(core.spellName .. " part of " .. GetAchievementLink(core.currentAchievementID) .. " FAILED! by (" .. core.destName .. ")")
 		arcaneAnnihilatorFailed = true
 	end
 
 	--Static Shock
 	if (core.type == "SPELL_ABSORBED" or core.type == "SPELL_DAMAGE" or core.type == "SPELL_MISSED") and core.spellId == 79912 and staticShockFailed == false then
-		SendChatMessage("[WIP] " .. core.spellName .. " part of " .. GetAchievementLink(5307) .. " FAILED! by (" .. core.destName .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+		core:sendMessage(core.spellName .. " part of " .. GetAchievementLink(core.currentAchievementID) .. " FAILED! by (" .. core.destName .. ")")
 		staticShockFailed = true
 	end
 
 	--Poison Bomb
 	if (core.type == "SPELL_DAMAGE" or core.type == "SPELL_MISSED") and core.spellId == 80092 and poisonBombFailed == false then
-		SendChatMessage("[WIP] " .. core.spellName .. " part of " .. GetAchievementLink(5307) .. " FAILED! by (" .. core.destName .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+		core:sendMessage(core.spellName .. " part of " .. GetAchievementLink(core.currentAchievementID) .. " FAILED! by (" .. core.destName .. ")")
 		poisonBombFailed = true
 	end
 
@@ -87,7 +83,7 @@ function core.BlackwingDescent:OminitronDefenseSystem()
 		else
 			if core.destName ~= flameThrowerPlayer then
 				--More than one person has got hit by the flamethrower
-				SendChatMessage("[WIP] " .. core.spellName .. " part of " .. GetAchievementLink(5307) .. " FAILED! by (" .. core.destName .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+				core:sendMessage(core.spellName .. " part of " .. GetAchievementLink(core.currentAchievementID) .. " FAILED! by (" .. core.destName .. ")")
 				flameThrowerFailed = true
 			end
 		end	
@@ -95,8 +91,6 @@ function core.BlackwingDescent:OminitronDefenseSystem()
 end
 
 function core.BlackwingDescent:Maloriak()
-	core:displayAchievementsToTrackCurrent(5310)
-
     if core.destID == "41440" and core.type == "UNIT_DIED" then
         aberrationsList[core.spawn_uid_dest] = nil
         aberrationsCounter = aberrationsCounter - 1
@@ -105,17 +99,16 @@ function core.BlackwingDescent:Maloriak()
         end
         aberrationsCounterKilled = aberrationsCounterKilled + 1
 
-        if core.achievementCompleted == true and timerStarted == false then
-            --Check that the adds were killed in time
-            SendChatMessage("[WIP] Timer Started! 10 Seconds Remaining" ,core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID) 
+        if startTimer == true and timerStarted == false then
+			--Check that the adds were killed in time
+			core:sendMessage("Timer Started! 10 Seconds Remaining")
             timerStarted = true
             C_Timer.After(10, function()
-                if aberrationsCounterKilled >= 12 then
-                    SendChatMessage("[WIP] COMPLETED! Aberrations were killed in time (" .. aberrationsCounterKilled .. "/12)" ,core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID) 
-                else
-                    SendChatMessage("[WIP] FAILED! Aberrations were not killed in time (" .. aberrationsCounterKilled .. "/12)" ,core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID) 
+				if aberrationsCounterKilled >= 12 then
+					core:getAchievementSuccess()
+				else
+					core:sendMessage(GetAchievementLink(core.currentAchievementID) .. " FAILED! Aberrations were not killed in time (" .. aberrationsCounterKilled .. "/12)")
                     aberrationsCounterKilled = 0
-                    core.achievementCompleted = false
                     timerStarted = false
                 end
             end)
@@ -125,46 +118,37 @@ function core.BlackwingDescent:Maloriak()
     if core.sourceID == "41440" and core.achievementCompleted == false then
         if aberrationsList[core.spawn_uid] == nil then
             aberrationsList[core.spawn_uid] = spawn_uid
-            aberrationsCounter = aberrationsCounter + 1
-            SendChatMessage("[WIP] Aberrations: " .. aberrationsCounter,core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)            
+			aberrationsCounter = aberrationsCounter + 1            
         end
     end
 
-    if aberrationsCounter == 12 and core.achievementCompleted == false then
-        SendChatMessage("[WIP] "  .. GetAchievementLink(5310) .. " requirements have been met. Adds can now be killed!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-        core.achievementCompleted = true
+	if aberrationsCounter == 12 and startTimer == false then
+		core:sendMessage(GetAchievementLink(core.currentAchievementID) .. " requirements have been met. Adds can now be killed!")
+		startTimer = true
     end	
 end
 
 function core.BlackwingDescent:Chimaeron()
-	core:displayAchievementsToTrackCurrent(5309)
-
 	if core.type == "UNIT_DIED" and core.unitType == "Player" then
 		playersDead = playersDead + 1
 	end
 
-	if playersDead == 3 and core.achievementFailed == false then
-		SendChatMessage("[WIP] "  .. GetAchievementLink(5309) .. " FAILED!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-		core.achievementFailed = true	
+	if playersDead == 3 then
+		core:getAchievementFailed()
 	end
 end
 
 function core.BlackwingDescent:Atramedes()
-	core:displayAchievementsToTrackCurrent(5308)
-
 	f:SetScript("OnEvent", function(self, event, unit, powerType)
 		if event == "UNIT_POWER" and powerType == "ALTERNATE" then
-			if UnitPower(unit, ALTERNATE_POWER_INDEX) > 50 and core.achievementFailed == false then
-				SendChatMessage("[WIP] "  .. GetAchievementLink(5308) .. " FAILED! by (" .. UnitName(unit) .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-				core.achievementFailed = true
+			if UnitPower(unit, ALTERNATE_POWER_INDEX) > 50 then
+				core:getAchievementFailedWithMessageAfter("by (" .. UnitName(unit) .. ")")
 			end
 		end
 	end)
 end
 
 function core.BlackwingDescent:Nefarian()
-	core:displayAchievementsToTrackCurrent(4849)
-
 	--Get percentage of onyxia and nefarian health
 	local onyxiaPercentage = nil
 	local nefarianPercentage = nil
@@ -182,37 +166,35 @@ function core.BlackwingDescent:Nefarian()
 	end
 
 	if step1Announced == false then
-		SendChatMessage("[WIP] Step 1: Damage Nefarian to below 65% but higher than 50%. Do not damge Onyxia",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+		core:sendMessage("Step 1: Damage Nefarian to below 65% but higher than 50%. Do not damge Onyxia")
 		step1Announced = true
 	end
 
 	if nefarianPercentage ~= nil and core.achievementFailed == false then
 		--Step 1: Damage Nefarian to atleast 60% but not lower than 50%
 		if nefarianPercentage > 50 and nefarianPercentage < 65 and onyxiaPercentage > 0 and step1Complete == false then
-			SendChatMessage("[WIP] Step 1: Complete",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+			core:sendMessage("Step 1: Complete")
 			step1Complete = true
-			SendChatMessage("[WIP] Step 2: Kill Onyxia",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+			core:sendMessage("Step 2: Kill Onyxia")
 		elseif nefarianPercentage > 65 and onyxiaPercentage == 0 and step1Complete == false then
 			--If onyxia is killed before nefarian reaches step1 threshold then fail achievement
-			SendChatMessage("[WIP] "  .. GetAchievementLink(4849) .. " FAILED! Do not kill onyxia yet",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-			core.achievementFailed = true		
+			core:getAchievementFailedWithMessageAfter("Do not kill onyxia yet")	
 		end 
 		
 		--Step 2: Kill Onyxia
 		if step1Complete == true and onyxiaPercentage == 0 and nefarianPercentage > 50 and step2Complete == false then
-			SendChatMessage("[WIP] Step 2: Complete",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+			core:sendMessage("Step 2: Complete")
 			step2Complete = true
-			SendChatMessage("[WIP] Step 3: Damage Nefarian to below 50% once on platform but do not kill him",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+			core:sendMessage("Step 3: Damage Nefarian to below 50% once on platform but do not kill him")
 		elseif nefarianPercentage < 50 and step2Complete == false then
-			SendChatMessage("[WIP] "  .. GetAchievementLink(4849) .. " FAILED! do not damage Nefrian below 50%",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-			core.achievementFailed = true			
+			core:getAchievementFailedWithMessageAfter("do not damage Nefrian below 50%")			
 		end
 
 		--Step 3: Damage Nefarian to below 50% once on platforms
 		if step2Complete == true and nefarianPercentage < 50 and step3Complete == false then
-			SendChatMessage("[WIP] Step 3: Complete",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+			core:sendMessage("Step 3: Complete")
 			step3Complete = true
-			SendChatMessage("[WIP] Step 4: When Nefarian has landed kill him",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)			
+			core:sendMessage("Step 4: When Nefarian has landed kill him")			
 		end
 
 		--Step 4: When landed kill Nefarian
@@ -220,8 +202,8 @@ function core.BlackwingDescent:Nefarian()
 			if event == "CHAT_MSG_MONSTER_YELL" and step3Complete == true and step4Complete == false then
 				if string.find(message, "I have tried to be an accommodating host, but you simply will not die! Time to throw all pretense aside and just... KILL YOU ALL!") then
 					C_Timer.After(3, function()
-						SendChatMessage("[WIP] Step 4: Complete",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-						SendChatMessage("[WIP] "  .. GetAchievementLink(4849) .. " requirements have been met. Boss can now be killed!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)							
+						core:sendMessage("Step 4: Complete")
+						core:getAchievementSuccess()							
 					end)
 					step4Complete = true
 				end
@@ -231,7 +213,7 @@ function core.BlackwingDescent:Nefarian()
 	end
 end
 
-function BlackwingDescent_ClearVariables()
+function core.BlackwingDescent:ClearVariables()
 	------------------------------------------------------
 	---- Ominitron Defense System
 	------------------------------------------------------
