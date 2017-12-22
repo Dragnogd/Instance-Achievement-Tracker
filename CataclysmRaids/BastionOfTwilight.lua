c8dd8dab9e288f146898d349a8d818787ee92264
Ryan Coombes <ryan.coombes1@ntlworld.com>
2017-12-22 09:25:16 +0000

    Cleaned up code

--- CataclysmRaids/BastionOfTwilight.lua
+++ CataclysmRaids/BastionOfTwilight.lua
@@ -23,19 +23,15 @@ local secondAddKilled = false
 local TwilightFiendsKilled = 0
 
 function core.BastionOfTwilight:HalfusWyrmbreaker()
-	core:displayAchievementsToTrackCurrent(5300)
-
 	if core.type == "UNIT_DIED" and core.destID == "44650" or core.destID == "44797" or core.destID == "44645" or core.destID == "44652" then
-		if timerStarted == false and (core.achievementCompleted == false or core.achievementFailed == false) then
-			SendChatMessage("[WIP] Timer Started! 10 Seconds Remaining" ,core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID) 
+		if timerStarted == false then
+			core:sendMessage("Timer Started! 10 Seconds Remaining")
 			timerStarted = true
             C_Timer.After(10, function()
 				if secondAddKilled == true then
-					SendChatMessage("[WIP] "  .. GetAchievementLink(5300) .. " requirements have been met. Boss can now be killed!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-					core.achievementCompleted = true
+					core:getAchievementSuccess()
 				else
-					SendChatMessage("[WIP] "  .. GetAchievementLink(5300) .. " FAILED!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-					core.achievementFailed = true						
+					core:getAchievementFailed()					
 				end
             end)
 		else
@@ -45,33 +41,27 @@ function core.BastionOfTwilight:HalfusWyrmbreaker()
 end
 
 function core.BastionOfTwilight_ValionaAndTheralion()
-	core:displayAchievementsToTrackCurrent(4852)
-
 	if core.type == "UNIT_DIED" and core.destID == "49864" then
 		TwilightFiendsKilled = TwilightFiendsKilled + 1
-		SendChatMessage("[WIP] (" .. TwilightFiendsKilled .. " /6) Twilight Fiends Killed",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
+		core:sendMessage("(" .. TwilightFiendsKilled .. " /6) Twilight Fiends Killed")
 	end
 
-	if TwilightFiendsKilled == 6 and core.achievementCompleted == false then
-		SendChatMessage("[WIP] "  .. GetAchievementLink(4852) .. " requirements have been met. Boss can now be killed!",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-		core.achievementCompleted = true		
+	if TwilightFiendsKilled == 6 then
+		core:getAchievementSuccess()
 	end
 end
 
 function core.BastionOfTwilight:Chogall()
-	core:displayAchievementsToTrackCurrent(5312)
-	
 	f:SetScript("OnEvent", function(self, event, unit, powerType)
 		if event == "UNIT_POWER" and powerType == "ALTERNATE" then
-			if UnitPower(unit, ALTERNATE_POWER_INDEX) > 30 and core.achievementFailed == false then
-				SendChatMessage("[WIP] "  .. GetAchievementLink(5312) .. " FAILED! by (" .. UnitName(unit) .. ")",core.chatType,DEFAULT_CHAT_FRAME.editBox.languageID)
-				core.achievementFailed = true
+			if UnitPower(unit, ALTERNATE_POWER_INDEX) > 30 then
+				core:getAchievementFailedWithMessageAfter("by (" .. UnitName(unit) .. ")")
 			end
 		end
 	end)	
 end
 
-function BastionOfTwilight_ClearVariables(type, destID)
+function core.BastionOfTwilight:ClearVariables()
 	------------------------------------------------------
 	---- Halfus Wyrmbreaker
 	------------------------------------------------------
