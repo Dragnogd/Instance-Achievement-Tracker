ec7c2ec85f775012d0a3aa0e19d9b21c8748bc39
Ryan Coombes <ryan.coombes1@ntlworld.com>
2017-12-24 22:19:50 +0000

    Started tracking on Scarlet Monastery and Scarlet Halls

--- PandariaDungeons/ScarletHalls.lua
+++ PandariaDungeons/ScarletHalls.lua
@@ -7,3 +7,19 @@ local _, core = ...
 ---- Scarlet Halls Bosses
 ------------------------------------------------------
 core.ScarletHalls = {}
+
+function core.ScarletHalls:HoundmasterBraun()
+    if core.type == "UNIT_DIED" and core.destID == "58876" then
+        print("Achievement Failed")
+    elseif core.destID == "58876" and core.overkill > 0 then
+        print("Achievement Failed")
+    end
+
+    if core.type == "UNIT_DIED" then
+        print(core.destName .. " : " .. core.destID)
+    end
+end
+
+function core.ScarletHalls:ClearVariables()
+
+end
--- PandariaDungeons/ScarletMonastery.lua
+++ PandariaDungeons/ScarletMonastery.lua
@@ -7,3 +7,45 @@ local _, core = ...
 ---- Scarlet Monastery Bosses
 ------------------------------------------------------
 core.ScarletMonastery = {}
+
+------------------------------------------------------
+---- Thalnos the Soulrender
+------------------------------------------------------
+local empoweredZombieCounter = 0
+local empoweredZombieTable = {}
+
+function core.ScarletMonastery:ThalnosTheSoulrender()
+    if core.type == "SPELL_SUMMON" and core.destID == "59930" and empoweredZombieTable[core.spawn_uid_dest] == nil then
+        empoweredZombieCounter = empoweredZombieCounter + 1
+        empoweredZombieTable[core.spawn_uid_dest] = core.spawn_uid_dest
+
+        if empoweredZombieCounter < 3 then
+            core:sendMessage("Empowered Zombie Counter (" .. empoweredZombieCounter .. "/3)")
+        end
+    end
+
+    if core.type == "UNIT_DIED" and core.destID == "59930" then
+        empoweredZombieCounter = empoweredZombieCounter - 1
+        empoweredZombieTable[core.spawn_uid_dest] = nil
+
+        if empoweredZombieCounter < 3 then
+            core:sendMessage("Empowered Zombie Counter (" .. empoweredZombieCounter .. "/3)")
+        end
+
+        if core.achievementCompleted == true and empoweredZombieCounter < 3 then
+            core:sendMessage("Empowered Zombie has died. DO NOT KILL BOSS (" .. empoweredZombieCounter .. "/3)")
+            core.achievementCompleted = false
+        end
+    end
+
+    if empoweredZombieCounter == 3 then
+        core:getAchievementSuccess()
+    end
+end
+
+function core.ScarletMonastery:ClearVariables()
+    ------------------------------------------------------
+    ---- Thalnos the Soulrender
+    ------------------------------------------------------
+    empoweredZombieCounter = 0
+end
