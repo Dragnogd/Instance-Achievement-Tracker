ee8b966530d14bde728fddff11d1b1e4cf8cd82f
Ryan Coombes <ryan.coombes1@ntlworld.com>
2017-12-29 20:15:06 +0000

    Added tracking for Skyreach

--- AchievementTrackerCore.lua
+++ AchievementTrackerCore.lua
@@ -328,7 +328,7 @@ function events:INSPECT_ACHIEVEMENT_READY()
 
 								--If the player has not completed the achievement then add them to the players string to display in the GUI
 								--Temp: will show completed achievements in GUI since I've already completed all the achievements
-								if completed ~= true then
+								if completed ~= false then
 									local name, _ = UnitName(playersToScan[1])
 									table.insert(core.Instances[expansion][instanceType][instance][boss].players, name)
 								end
--- DraenorDungeons/Skyreach.lua
+++ DraenorDungeons/Skyreach.lua
@@ -7,3 +7,62 @@ local _, core = ...
 ---- Skyreach Bosses
 ------------------------------------------------------
 core.Skyreach = {}
+
+------------------------------------------------------
+---- Ranjit
+------------------------------------------------------
+local playersHit = {}
+
+function core.Skyreach:Ranjit()
+    local playerHit = false
+    local reason = nil
+    
+    --Spinning Blade
+    if core.spellId == 153123 then 
+        playerHit = true
+        reason = "Spinning Blade"
+    end
+
+    --Windwall
+    if core.spellId == 153759 then
+        playerHit = true
+        reason = "Windwall"
+    end
+    
+    --Four Winds
+    if core.spellId == 153139 then
+        playerHit = true
+        reason = "Four Winds"
+    end
+    
+    --Lens Flare
+    if core.spellId == 154043 then
+        playerHit = true
+        reason = "Lens Flare"
+    end
+    
+    if playerHit == true then
+        if playersHit[core.destName] == nil then
+            --Players has not been hit already
+            --Check if the player actually needs the achievement
+            if core:has_value(core.Instances.WarlordsOfDraenor.Dungeons.Skyreach.boss1.players, core.destName) then
+                --Player needs achievement but has failed it
+                core:sendMessage(core.destName .. " has failed " .. GetAchievementLink(core.currentAchievementID) .. " (Personal Achievement) (Reason: Damage from " .. reason .. ")")
+            end
+            playersHit[core.destName] = true
+        end
+    end
+end
+
+function core.Skyreach:HighSageViryx()
+    if core.type == "UNIT_DIED" and core.destID == "76292" then
+        core:getAchievementFailed()
+    end
+end
+
+function core.Skyreach:ClearVariables()
+    ------------------------------------------------------
+    ---- Ranjit
+    ------------------------------------------------------
+    playersHit = {}
+end
