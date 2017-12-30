3c59ce9f0bfcffb5d250fed29d1d0181b5c664af
Ryan Coombes <ryan.coombes1@ntlworld.com>
2017-12-30 20:01:07 +0000

    Added tracking for Bloodmaul Slag Mines

--- DraenorDungeons/BloodmaulSlagMines.lua
+++ DraenorDungeons/BloodmaulSlagMines.lua
@@ -7,3 +7,21 @@ local _, core = ...
 ---- Bloodmaul Slag Mines Bosses
 ------------------------------------------------------
 core.BloodmaulSlagMines = {}
+
+function core.BloodmaulSlagMines:Magmolatus()
+    if core.type == "UNIT_DIED" and core.destID == "74571" then
+        core:getAchievementFailedWithMessageAfter("(Reason: Calamity has died)")
+    elseif core.type == "UNIT_DIED" and core.destID == "74570" then
+        core:getAchievementFailedWithMessageAfter("(Reason: Ruination has died)")
+    end
+end
+
+function core.BloodmaulSlagMines:Gugrokk()
+    if core.type == "UNIT_DIED" and core.destID == "74927" then
+        core:getAchievementFailed()
+    end
+end
+
+function core.BloodmaulSlagMines:ClearVariables()
+
+end
