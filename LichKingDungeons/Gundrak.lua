fe2496f8dded62413d0063ddacc241cc426d9f42
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-01-27 16:50:22 +0000

    Added tracking for Gundrak

--- LichKingDungeons/Gundrak.lua
+++ LichKingDungeons/Gundrak.lua
@@ -7,3 +7,22 @@ local _, core = ...
 ---- Gundrak Bosses
 ------------------------------------------------------
 core.Gundrak = {}
+
+function core.Gundrak:Sladran()
+    --Boss does not fire Encounter Start
+    core:getAchievementToTrack()
+
+    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 55126 then
+        core:getAchievementFailed()
+    end
+end
+
+function core.Gundrak:Moorabi()
+    if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 55098 then
+        core:getAchievementFailed()
+    end
+end
+
+function core.Gundrak:ClearVariables()
+
+end
