1175848d06850597a93b0223c2459c7039753083
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-01-28 19:16:48 +0000

    Added tracking for Azjol Nerub

--- LichKingDungeons/AzjolNerub.lua
+++ LichKingDungeons/AzjolNerub.lua
@@ -7,3 +7,43 @@ local _, core = ...
 ---- Azjol-Nerub Bosses
 ------------------------------------------------------
 core.AzjolNerub = {}
+
+------------------------------------------------------
+---- Watch Him Die
+------------------------------------------------------
+local achievementFailed = false
+
+------------------------------------------------------
+---- Anub'arak
+------------------------------------------------------
+local timerStarted = false
+local timer
+
+function core.AzjolNerub:TrackAdditional()
+    --Watch Him Die
+    if core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].enabled == true then
+        if core.type == "UNIT_DIED" and (core.destID == "28729" or core.destID == "28730" or core.destID == "28731") and achievementFailed == false then
+            achievementFailed = true
+            core:sendMessage(GetAchievementLink(1296) .. " FAILED!")
+        end
+    end
+end
+
+function core.AzjolNerub:Anubarak()
+    if timerStarted == false then
+        timerStarted = true
+        timer = C_Timer.NewTimer(241, function() 
+            core:getAchievementFailed()
+        end)
+    end
+end
+
+function core.AzjolNerub:ClearVariables()
+    ------------------------------------------------------
+    ---- Anub'arak
+    ------------------------------------------------------
+    timerStarted = false
+    if timer ~= nil then
+        timer:Cancel()
+    end
+end
