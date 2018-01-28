afbacfd57e72187f791e9c6f9e03d5b3306c2a61
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-01-28 17:37:54 +0000

    Added tracking for Utgarde Keep

--- LichKingDungeons/HallsOfReflection.lua
+++ LichKingDungeons/HallsOfReflection.lua
@@ -7,3 +7,7 @@ local _, core = ...
 ---- Halls of Reflection Bosses
 ------------------------------------------------------
 core.HallsOfReflection = {}
+
+function core.HallsOfReflection:ClearVariables()
+
+end
--- LichKingDungeons/UtgardeKeep.lua
+++ LichKingDungeons/UtgardeKeep.lua
@@ -8,6 +8,14 @@ local _, core = ...
 ------------------------------------------------------
 core.UtgardeKeep = {}
 
+function core.UtgardeKeep:PrinceKeleseth()
+    if core.destID == "23965" and core.overkill ~= nil then
+        if core.overkill > 0 then
+            core:getAchievementFailedWithMessageAfter("(" .. core.sourceName .. ")")
+        end
+    end
+end
+
 function core.UtgardeKeep:ClearVariables()
     
 end
