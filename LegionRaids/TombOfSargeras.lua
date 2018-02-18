385c5e95c6763cb0d8db548e409c61710b2a51e7
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-02-18 16:14:50 +0000

    Added tracking for Tomb of Sargeras

--- LegionRaids/TombOfSargeras.lua
+++ LegionRaids/TombOfSargeras.lua
@@ -7,3 +7,47 @@ local _, core = ...
 ---- Tomb of Sargeras Bosses
 ------------------------------------------------------
 core.TombOfSargeras = {}
+
+------------------------------------------------------
+---- Sisters of the Moon
+------------------------------------------------------
+local healthPercentageReached = false
+
+function core.TombOfSargeras:Harjatan()
+    if core.type == "UNIT_DIED" and core.destID == "121071" then
+        core:getAchievementSuccess()
+    end
+end
+
+function core.TombOfSargeras:SistersOfTheMoon()
+    --Check if boss has reached 20% health
+    if UnitHealth("boss1") / UnitHealthMax("boss") *100 <= 20 then
+        healthPercentageReached = true
+
+        if healthPercentageReached == false then
+            print("Health Percentage Reached")
+        end
+    end
+
+    --Add has died
+    if core.type == "UNIT_DIED" and core.destID == "121498" then
+        if healthPercentageReached == true then
+            core:getAchievementSuccess()
+        else
+            core:getAchievementFailed()
+        end
+    end
+end
+
+function core.TombOfSargeras:Kiljaeden()
+    if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 241702 then
+        core:getAchievementFailed()
+    end
+end
+
+function core.TombOfSargeras:ClearVariables()
+    ------------------------------------------------------
+    ---- Sisters of the Moon
+    ------------------------------------------------------
+    healthPercentageReached = false
+end
