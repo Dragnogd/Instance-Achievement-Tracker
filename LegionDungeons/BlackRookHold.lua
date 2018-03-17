b6d8717658db80a4afd8dbc25607db446114aa59
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-03-17 12:18:50 +0000

    Added tactics for Black Rook Hold

--- LegionDungeons/BlackRookHold.lua
+++ LegionDungeons/BlackRookHold.lua
@@ -7,3 +7,18 @@ local _, core = ...
 ---- Black Rook Hold Bosses
 ------------------------------------------------------
 core.BlackRookHold = {}
+
+function core.BlackRookHold:AmalgamOfSouls()
+    if core.type == "UNIT_DIED" and core.destID == "99663" then
+        core:getAchievementSuccess()
+    end
+end
+
+function core.BlackRookHold:IllysannaRavencrest()
+    if core.type == "UNIT_DIED" and core.destID == "100485" then
+        core:getAchievementFailed()
+    end
+end
+
+function core.BlackRookHold:ClearVariables()
+end
