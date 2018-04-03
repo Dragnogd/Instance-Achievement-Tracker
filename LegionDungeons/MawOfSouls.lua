524f9c2477b253bdd6eefee362a4f1bb3857015c
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-03 22:01:15 +0100

    Altered tracking for Instant Karma

--- LegionDungeons/MawOfSouls.lua
+++ LegionDungeons/MawOfSouls.lua
@@ -12,15 +12,28 @@ core.MawOfSouls = {}
 ---- Ymiron
 ------------------------------------------------------
 local risenWarriorsKilled = 0
+local defiantStrikeActive = false
 
 function core.MawOfSouls:Ymiron()
-    if core.type == "UNIT_DIED" and core.destID == "98246" and core.destFlags == 0xa48 then
-        risenWarriorsKilled = risenWarriorsKilled + 1
-        core:sendMessage(core:getAchievement() .. " Risen Warriors Killed (" .. risenWarriorsKilled .. "/6)")
+    if core.type == "SPELL_AURA_APPLIED" and core.destID == "97200" and core.spellId == 195031 then
+        defiantStrikeActive = true
     end
 
-    if risenWarriorsKilled >= 6 then
-        core:getAchievementSuccess()
+    if core.type == "SPELL_AURA_REMOVED" and core.destID == "97200" and core.spellId == 195031 then
+        defiantStrikeActive = false
+        risenWarriorsKilled = 0
+    end
+
+    --Only count UNIT_DIED kills when defiant strike is active
+    if defiantStrikeActive == true then
+        if core.type == "UNIT_DIED" and core.destID == "98246" and core.destFlags == 0xa48 then
+            risenWarriorsKilled = risenWarriorsKilled + 1
+            core:sendMessage(core:getAchievement() .. " Risen Warriors Killed (" .. risenWarriorsKilled .. "/6)")
+        end
+    
+        if risenWarriorsKilled >= 6 then
+            core:getAchievementSuccess()
+        end      
     end
 end
 
@@ -29,4 +42,5 @@ function core.MawOfSouls:ClearVariables()
     ---- Ymiron
     ------------------------------------------------------
     risenWarriorsKilled = 0
+    defiantStrikeActive = false
 end
