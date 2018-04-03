317b162d205138fafdfccf72abb8cade75964343
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-03 22:06:45 +0100

    Added tracking for Can't Eat Just One

--- LegionDungeons/NeltharionsLair.lua
+++ LegionDungeons/NeltharionsLair.lua
@@ -7,3 +7,12 @@ local _, core = ...
 ---- Neltharions Lair Bosses
 ------------------------------------------------------
 core.NeltharionsLair = {}
+
+function core.NeltharionsLair:Naraxas()
+    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.destID == "91005" and core.spellId == 199246 and core.amount == 6 then
+        core:getAchievementSuccess()
+    end
+end
+
+function core.NeltharionsLair:ClearVariables()
+end
