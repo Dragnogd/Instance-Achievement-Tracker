211d6a6edd3bd96eb8ed0c39079eed67b1067b72
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-08 10:49:31 +0100

    Added additional checks to Militaristic, Expansionist achievement

--- DraenorDungeons/IronDocks.lua
+++ DraenorDungeons/IronDocks.lua
@@ -14,38 +14,46 @@ core.IronDocks = {}
 local SiegemasterOlugarFound = false
 local PitwardenGwarnokFound = false
 local ChampionDrunaFound = false
+local fleshrenderKilled = false
 
 
 function core.IronDocks:FleshrenderNokgar()
-    --Siegemaster Olugar
-    if (core.sourceID == "83026" or core.destID == "83026") and SiegemasterOlugarFound == false then
-        SiegemasterOlugarFound = true
+    --If Fleshrender Nok'gar has died then stop tracking
+    if core.type == "UNIT_DIED" and core.destID == "81305" then
+        fleshrenderKilled = true
     end
 
-    --Pitwarden Gwarnok
-    if (core.sourceID == "84520" or core.destID == "84520") and PitwardenGwarnokFound == false then
-        PitwardenGwarnokFound = true
-    end
-
-    --Champion Druna
-    if (core.sourceID == "81603" or core.destID == "81603") and ChampionDrunaFound == false then
-        ChampionDrunaFound = true
-    end
-
-    if core.type == "UNIT_DIED" and (core.destID == "83026" or core.destID == "84520" or core.destID == "81603") then
-        local reason = nil
-        if core.destID == "83026" then
-            reason = "Siegemaster Olugar"
-        elseif core.destID == "84520" then
-            reason = "Pitwarden Gwarnok"
-        elseif core.destID == "81603" then
-            reason = "Champion Druna"
+    if fleshrenderKilled == false then
+        --Siegemaster Olugar
+        if (core.sourceID == "83026" or core.destID == "83026") and SiegemasterOlugarFound == false then
+            SiegemasterOlugarFound = true
         end
-        core:getAchievementFailedWithMessageAfter("(Reason: " .. reason .. " has died)")
-    end
 
-    if (core.sourceID == "81305" or core.destID == "81305") and SiegemasterOlugarFound == true and PitwardenGwarnokFound == true and ChampionDrunaFound == true then
-        core:getAchievementSuccess()
+        --Pitwarden Gwarnok
+        if (core.sourceID == "84520" or core.destID == "84520") and PitwardenGwarnokFound == false then
+            PitwardenGwarnokFound = true
+        end
+
+        --Champion Druna
+        if (core.sourceID == "81603" or core.destID == "81603") and ChampionDrunaFound == false then
+            ChampionDrunaFound = true
+        end
+
+        if core.type == "UNIT_DIED" and (core.destID == "83026" or core.destID == "84520" or core.destID == "81603") then
+            local reason = nil
+            if core.destID == "83026" then
+                reason = "Siegemaster Olugar"
+            elseif core.destID == "84520" then
+                reason = "Pitwarden Gwarnok"
+            elseif core.destID == "81603" then
+                reason = "Champion Druna"
+            end
+            core:getAchievementFailedWithMessageAfter("(Reason: " .. reason .. " has died)")
+        end
+
+        if (core.sourceID == "81305" or core.destID == "81305") and SiegemasterOlugarFound == true and PitwardenGwarnokFound == true and ChampionDrunaFound == true then
+            core:getAchievementSuccess()
+        end
     end
 end
 
