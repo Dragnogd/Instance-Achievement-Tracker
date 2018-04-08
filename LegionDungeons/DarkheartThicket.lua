42b9fb6c0a996f11c33eae4b92860ae6f8147dda
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-08 15:15:50 +0100

    Possible fix for incorrect tracking for Burning Down the House

--- LegionDungeons/DarkheartThicket.lua
+++ LegionDungeons/DarkheartThicket.lua
@@ -8,6 +8,11 @@ local _, core = ...
 ------------------------------------------------------
 core.DarkheartThicket = {}
 
+------------------------------------------------------
+---- Shade of Xavius
+------------------------------------------------------
+local shadeOfXaviusKilled = false
+
 function core.DarkheartThicket:Dresaron()
     if core.type == "UNIT_DIED" and core.destID == "111008" then
         core:getAchievementSuccess()
@@ -15,6 +20,11 @@ function core.DarkheartThicket:Dresaron()
 end
 
 function core.DarkheartThicket:ShadeOfXavius()
+    --Check if boss has died
+    if core.type == "UNIT_DIED" and core.destID == "99192" then
+        shadeOfXaviusKilled = true
+    end
+
     --Boss has 10 stacks of Apocalyptic Empowerment
     if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount == 10 and core.spellId == 221315 then
         core.achievementsFailed[1] = false
@@ -22,7 +32,7 @@ function core.DarkheartThicket:ShadeOfXavius()
     end
 
     --Apocalyptic Empowerment fell off boss after requirements have been met
-    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 221315 and core.achievementsCompleted[1] == true and core.inCombat == true then
+    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 221315 and core.achievementsCompleted[1] == true and shadeOfXaviusKilled == false then
         core.achievementsCompleted[1] = false
         core:getAchievementFailedWithMessageAfter("Apocalyptic Empowerment stacks fallen off boss")
     end
