703b8ca08f15a614e2e413eb5034aabba2997523
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-14 20:52:48 +0100

    Changes Helya

--- LegionRaids/TrialOfValor.lua
+++ LegionRaids/TrialOfValor.lua
@@ -9,16 +9,12 @@ local _, core = ...
 core.TrialOfValor = {}
 
 ------------------------------------------------------
----- Helya Killed
+---- Helya
 ------------------------------------------------------
 local fetidNames = {}
 local fetidcount = 0
 
 function core.TrialOfValor:Helya()
-    if core.groupSize == (fetidcount - 1) then
-        core:getAchievementSuccess()
-    end
-
     if core.type == "SPELL_AURA_APPLIED" and core.spellId == 193367 then
         if fetidNames[core.destName] == nil then
             fetidNames[core.destName] = core.destName
@@ -27,18 +23,22 @@ function core.TrialOfValor:Helya()
         end
     end
 
+    if core.groupSize == (fetidcount - 1) then
+        core:getAchievementSuccess()
+    end
+
     if core.type == "SPELL_AURA_REMOVED" and core.spellId == 193367 then
         if fetidNames[core.destName] ~= nil then
             fetidNames[core.destName] = nil
             fetidcount = fetidcount - 1
             --core:sendMessage(core.destName .. " Lost Fetid Rot (" .. fetidcount .. "/" .. core.groupSize .. ")")
         end
-    end    
+    end  
 end
 
 function core.TrialOfValor:ClearVariables()
     ------------------------------------------------------
-    ---- Helya Killed
+    ---- Helya
     ------------------------------------------------------
     fetidNames = {}
     fetidcount = 0
