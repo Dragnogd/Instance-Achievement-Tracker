3733c42beadbe3faa4ffda0414e6b4e676388454
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-14 10:23:47 +0100

    Added tracking for Arcanic Cling

--- LegionDungeons/TheArcway.lua
+++ LegionDungeons/TheArcway.lua
@@ -7,3 +7,20 @@ local _, core = ...
 ---- The Arcway Bosses
 ------------------------------------------------------
 core.TheArcway = {}
+
+function core.TheArcway:Ivanyr()
+    if core.type == "SPELL_DAMAGE" and core.spellId == 220581 then
+        core:getAchievementFailedPersonal()
+    end
+end
+
+function core.TheArcway:Corstilax()
+
+end
+
+function core.TheArcway:AdvisorVandros()
+    
+end
+
+function core.TheArcway:ClearVariables()
+end
