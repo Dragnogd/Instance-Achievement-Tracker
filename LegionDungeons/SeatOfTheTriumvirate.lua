56f4b73570d37709779adfc403137f5892747a3e
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-27 21:08:47 +0100

    Added tracking for Darker Side

--- LegionDungeons/SeatOfTheTriumvirate.lua
+++ LegionDungeons/SeatOfTheTriumvirate.lua
@@ -8,9 +8,39 @@ local _, core = ...
 ------------------------------------------------------
 core.SeatOfTheTriumvirate = {}
 
+------------------------------------------------------
+---- L'ura
+------------------------------------------------------
+local crushingDarknessCounter = 0
+local luraKilled = false
+
 function core.SeatOfTheTriumvirate:ZuraalTheAscended()
     if core.type == "UNIT_DIED" and core.destID == "126283" then
         core:getAchievementSuccess()
+    end
+end
+
+function core.SeatOfTheTriumvirate:Lura()
+    if core.type == "UNIT_DIED" and core.destID == "124729" then
+        luraKilled = Triumvirate
+    end
+
+    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 250821 and luraKilled == false then
+        core:getAchievmentFailed()
+    end
+end
+
+function core.SeatOfTheTriumvirate:TrackAdditional()
+    --Gained Crushing Darkness
+    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 250821 then
+        crushingDarknessCounter = crushingDarknessCounter + 1
+        core:sendMessage(core.destName .. " Has gained Crushing Darkness (" .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
+    end
+
+    --Lost Crushing Darkness
+    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 250821 and luraKilled == false then
+        crushingDarknessCounter = crushingDarknessCounter - 1
+        core:sendMessage(core.destName .. " Has lost Crushing Darkness ( " .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
     end
 end
 
