0889474fddf268cd872c83a34e4844611aca0423
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-28 11:39:33 +0100

    Added tracking for I've Had Worse

--- LichKingDungeons/TrialOfTheChampion.lua
+++ LichKingDungeons/TrialOfTheChampion.lua
@@ -7,3 +7,12 @@ local _, core = ...
 ---- Trial of the Champion Bosses
 ------------------------------------------------------
 core.TrialOfTheChampion = {}
+
+function core.TrialOfTheChampion:TheBlackKnight()
+    if (core.type == "SPELL_DAMAGE" or core.type == "SPELL_MISSED") and core.spellId == 67729 then
+        core:getAchievementFailedWithMessageAfter("(" .. core.destName .. ")")
+    end
+end
+
+function core.TrialOfTheChampion:ClearVariables()
+end
