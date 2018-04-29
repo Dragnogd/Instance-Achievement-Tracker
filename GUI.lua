0c85163cdcccbdb1b1da44e210ac8fa2efc2abdd
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-04-29 13:54:45 +0100

    Release V1.0.0r

--- GUI.lua
+++ GUI.lua
@@ -24,7 +24,7 @@ AchievementTrackerOptions = {}
 Config.majorVersion = 1						--Addon with a higher major version change have priority over a lower major version
 Config.minorVersion = 0						--Addon with a minor version change have prioirty over a lower minor version
 Config.revisionVersion = 0					--Addon with a revision change have the same priorty as a lower revision verison
-Config.releaseType = "rn"                    --Release type (Alpha, Beta, Release)
+Config.releaseType = "r"                    --Release type (Alpha, Beta, Release)
 
 --------------------------------------
 -- Config functions
