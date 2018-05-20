9cb4ae5c35e74ea1c483ae99bf346facf6654627
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-05-20 16:08:27 +0100

    Added tracking for Not a Fun Guy and Taint Nobody Got Time For That

---
+++ BattleForAzerothDungeons/TheUnderrot.lua
@@ -0,0 +1,21 @@
+--------------------------------------
+-- Namespaces
+--------------------------------------
+local _, core = ...
+
+------------------------------------------------------
+---- The Underrot Bosses
+------------------------------------------------------
+core.TheUnderrot = {}
+
+function core.TheUnderrot:SporecallerZancha()
+    if core.type == "UNIT_DIED" and core.destID == "138740" then
+        core:getAchievementSuccess()
+    end
+end
+
+function core.TheUnderrot:ElderLeaxa()
+    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 260685 then
+        core:getAchievementFailedWithMessageAfter("(" .. core.destName .. ")")
+    end
+end
