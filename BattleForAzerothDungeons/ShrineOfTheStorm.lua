4d23cdc58348cdc7262d1ea74d11ad7d5e9660a4
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-07-21 15:38:16 +0100

    Added tracking for Trust No One

--- BattleForAzerothDungeons/ShrineOfTheStorm.lua
+++ BattleForAzerothDungeons/ShrineOfTheStorm.lua
@@ -7,3 +7,55 @@ local _, core = ...
 ---- Shrine of the Storm Bosses
 ------------------------------------------------------
 core.ShrineOfTheStorm = {}
+
+------------------------------------------------------
+---- Vol'zith the Whisperer
+------------------------------------------------------
+local maddeningDreamCount = 0
+local maddeningDreamPlayersUID = {}
+local volzithKilled = false
+
+function core.ShrineOfTheStorm:VolzithTheWhisperer()
+    --Defeat Vol'zith the Whisperer while all party members are afflicted with Maddening Dreams in Shrine of the Storm on Mythic difficulty.
+
+    if volzithKilled == false then
+        --Player gains Maddening Dreams
+        if core.type == "SPELL_AURA_APPLIED" and core.spellId == 275690 then
+            if maddeningDreamPlayersUID[core.destName] == nil then
+                maddeningDreamCount = maddeningDreamCount + 1
+                maddeningDreamPlayersUID[core.destName] = core.destName
+                core:sendMessage(core.destName .. " has gained Maddening Dreams (" .. maddeningDreamCount .. "/" .. core.groupSize .. ")")
+            end
+        end
+
+        --Player looses Maddening Dreams
+        if core.type == "SPELL_AURA_REMOVED" and core.spellId == 275690 then
+            if maddeningDreamPlayersUID[core.destName] ~= nil then
+                maddeningDreamCount = maddeningDreamCount - 1
+                maddeningDreamPlayersUID[core.destName] = nil
+                core:sendMessage(core.destName .. " has lost Maddening Dreams (" .. maddeningDreamCount .. "/" .. core.groupSize .. ")")
+
+                --If achievement was already completed then fail it
+                if core.achievementsCompleted[1] == true then
+                    core:getAchievementFailed()
+                    core.achievementsCompleted[1] = false
+                end
+            end
+        end
+
+        --Check if all players have Maddening Dreams
+        if maddeningDreamCount == core.groupSize then
+            core:getAchievementSuccess()
+            core.achievementsFailed[1] = false
+        end
+    end
+end
+
+function core.ShrineOfTheStorm:ClearVariables()
+    ------------------------------------------------------
+    ---- Vol'zith the Whisperer
+    ------------------------------------------------------
+    maddeningDreamCount = 0
+    maddeningDreamPlayersUID = {}
+    volzithKilled = false
+end
