2c88c42ccd54659e2d0d76c3a822a7f997fbe913
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-08-13 15:28:47 +0100

    Added tracking for Wrap God

--- BattleForAzerothDungeons/KingsRest.lua
+++ BattleForAzerothDungeons/KingsRest.lua
@@ -8,15 +8,39 @@ local _, core = ...
 ------------------------------------------------------
 core.KingsRest = {}
 
+------------------------------------------------------
+---- Mchimba The Embalmer
+------------------------------------------------------
+local struggleUsed = false
+
 function core.KingsRest:MchimbaTheEmbalmer()
     --Defeat Mchimba the Embalmer without him finishing a cast of Open Coffin and with entombed players using Struggle no more than once per cast of Entomb on Mythic Difficulty.
+
+    --Boss has successfully cast Open Coffin so fail achievement
+    if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 271290 then
+        core:getAchievementFailed()
+    end
+
+    --Reset Struggle counter since emtomb has been cast
+    if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 267702 then
+        struggleUsed = false
+    end
+
+    --Check if player has used Struggle too many times.
+    if core.type == "SPELL_CAST_SUCEESS" and core.spellId == 267764 and struggleUsed = false then
+        struggleUsed = true
+    else
+        core:getAchievementFailed()
+    end
 
     --Entomb 267702
     --Open Coffin 271290
     --Struggle 267764
-
-    --If boss casts open coffin then fail acheivement
 end
 
 function core.KingsRest:ClearVariables()
+    ------------------------------------------------------
+    ---- Mchimba The Embalmer
+    ------------------------------------------------------
+    struggleUsed = false
 end
