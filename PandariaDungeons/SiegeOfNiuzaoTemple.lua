c635fe0539eb3b5fab60394b3a13b8923c04ab69
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-08-27 17:02:02 +0100

    Fixed Run With The Wind

--- PandariaDungeons/SiegeOfNiuzaoTemple.lua
+++ PandariaDungeons/SiegeOfNiuzaoTemple.lua
@@ -38,27 +38,27 @@ end
 function core._1011:WingLeaderNeronok()
     --Recieved Buff
     if (core.type == "SPELL_AURA_APPLIED" or core.type == "SPELL_AURA_REFRESH") and core.spellId == 125146 then
-        if core:has_value(core.Instances.MistsOfPandaria.Dungeons._1011.boss3.players, core.destName) then
+        if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, core.destName) then
             --Player needs achievement but has failed it
             TenwusResolveCounter = TenwusResolveCounter + 1
-            core:sendMessage(core.destName .. " has recieved Tenwu's Resolve (" .. TenwusResolveCounter .. "/" .. #core.Instances.MistsOfPandaria.Dungeons._1011.boss3.players .. ") (Personal Achievement)")
+            core:sendMessage(core.destName .. " has recieved Tenwu's Resolve (" .. TenwusResolveCounter .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ") (Personal Achievement)")
             TenwusPlayerTrack[core.destName] = true
         end
     end 
     
     --Lost Buff
     if core.type == "SPELL_AURA_REMOVED" and core.spellId == 125146 and TenwusPlayerTrack[core.destName] ~= nil then
-        if core:has_value(core.Instances.MistsOfPandaria.Dungeons._1011.boss3.players, core.destName)  then
+        if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, core.destName)  then
             --Player needs achievement but has failed it
             TenwusResolveCounter = TenwusResolveCounter - 1
-            core:sendMessage(core.destName .. " has LOST Tenwu's Resolve. DO NOT KILL BOSS! (" .. TenwusResolveCounter .. "/" .. #core.Instances.MistsOfPandaria.Dungeons._1011.boss3.players .. ") (Personal Achievement)")
+            core:sendMessage(core.destName .. " has LOST Tenwu's Resolve. DO NOT KILL BOSS! (" .. TenwusResolveCounter .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ") (Personal Achievement)")
             core.achievementCompleted = false
             TenwusPlayerTrack[core.destName] = false
         end   
     end
 
     --Everyone who needs achievement has got it
-    if TenwusResolveCounter == #core.Instances.MistsOfPandaria.Dungeons._1011.boss3.players then
+    if TenwusResolveCounter == #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players then
         core:getAchievementSuccess()
     end
 end
