982e1786312c7c8cdc73afce8bce9adcdd842d1e
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-09-01 18:22:09 +0100

    Added tracking for Snake Eater

--- BattleForAzerothDungeons/TempleOfSethraliss.lua
+++ BattleForAzerothDungeons/TempleOfSethraliss.lua
@@ -7,3 +7,10 @@ local _, core = ...
 ---- Temple of Sethraliss Bosses
 ------------------------------------------------------
 core._1877 = {}
+
+function core._1877:Merektha()
+    --Detect solid snake kill and tell raid to loot the body
+    if core.type == "UNIT_DIED" and core.destID == "139304" then
+        core:sendMessage(core:getAchievement() .. " Loot the Solid Snake now to complete the achievement")
+    end
+end
