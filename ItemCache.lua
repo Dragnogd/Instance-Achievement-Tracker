40bd6d87b5a25111fd5eec15bcf794d85938e171
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-09-21 17:38:04 +0100

    Added tactics for Uldir

--- ItemCache.lua
+++ ItemCache.lua
@@ -7,4 +7,5 @@ core.ItemCache = {
     [119210] = 119210, --Hearthstone Board
     [143660] = 143660, --Mrgrglhjorn
     [129207] = 129207, --Aegis of Aggramar
+    [64482] = 64482, --Puzzle Box of Yogg-Saron
 }
