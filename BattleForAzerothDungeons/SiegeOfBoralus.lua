7bb1fd3968576e9f128f410356430daff1114775
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-09-23 18:00:47 +0100

    Added tracking for Stand By Me

--- BattleForAzerothDungeons/SiegeOfBoralus.lua
+++ BattleForAzerothDungeons/SiegeOfBoralus.lua
@@ -7,3 +7,9 @@ local _, core = ...
 ---- Siege of Boralus Bosses
 ------------------------------------------------------
 core._1822 = {}
+
+function core._1822:DreadCaptainLockwood()
+    if core:getBlizzardTrackingStatus(12727) == false then
+        core:getAchievementFailed()
+    end
+end
