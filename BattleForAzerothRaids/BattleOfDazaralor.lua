732207d17fde61dc0d5164caaad692a820f80e46
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-12-16 20:56:56 +0000

    Added tracking for We Got Spirit, How About You?

--- BattleForAzerothRaids/BattleOfDazaralor.lua
+++ BattleForAzerothRaids/BattleOfDazaralor.lua
@@ -2,8 +2,29 @@
 -- Namespaces
 --------------------------------------
 local _, core = ...
+local L = core.L
 
 ------------------------------------------------------
 ---- Battle of Dazar'alor Bosses
 ------------------------------------------------------
-core.BattleOfDazaralor = {}
+core._2070 = {}
+
+function core._2070:KingRastakhan()
+    --Defeat King Rastakhan with the Phantom of Retribution, Phantom of Rage and Phantom of Slaughter still alive on Normal difficulty or higher.
+
+    --Phantom of Retribution Died
+    if core.type == "UNIT_DIED" and core.destID == "146491" then
+        core:getAchievementFailed()
+    end
+
+    --Phantom of Rage
+    if core.type == "UNIT_DIED" and core.destID == "146492" then
+        core:getAchievementFailed()
+    end
+
+    --Phantom of Slaughter
+    if core.type == "UNIT_DIED" and core.destID == "146493" then
+        core:getAchievementFailed()
+    end
+end
+
