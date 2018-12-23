775d1a98c005da87eed57bc35046064154310b4b
Ryan Coombes <ryan.coombes1@ntlworld.com>
2018-12-23 16:27:51 +0000

    Updated enES locales

--- Localization.esES.lua
+++ Localization.esES.lua
@@ -13,16 +13,16 @@ local baseLocale = {
     ["GUI_Enabled"] = "Activado",
     ["GUI_Disabled"] = "Desactivado",
     ["GUI_ToggleMinimap"] = "Muestra el Botón del Minimapa",
-    -- ["GUI_MinimapEnabled"] = "Minimap icon enabled",
-    -- ["GUI_MinimapDisabled"] = "Minimap icon disabled",
-    -- ["GUI_AnnounceTracking"] = "Announce achievements being tracked to group", --L["Announce Achievements Being Tracked To Group"]
-    -- ["GUI_OnlyTrackMissingAchievements"] = "Only track missing achievements", --["Only track missing achievements"]
-    -- ["GUI_AnnounceMessagesToRaidWarning"] = "Announce messages to Raid Warning", --["Announce messages to Raid Warning"]
-    -- ["GUI_AchievementsDiscordTitle"] = "Achievements Discord",
-    -- ["GUI_AchievementsDiscordDescription"] = "Tactics kindly contributed by the Achievements Discord Server, the community where members can meet like-minded completionists and form groups for a variety of achievements.",
-    -- ["GUI_PlaySoundOnSuccess"] = "Play a sound when a achievement is completed",
-    -- ["GUI_PlaySoundOnFailed"] = "Play a sound when an achievement fails",
-    -- ["GUI_SelectSound"] = "Select Sound",
+    ["GUI_MinimapEnabled"] = "Icono de minimapa activado",
+    ["GUI_MinimapDisabled"] = "Icono de minimapa desactivado",
+    ["GUI_AnnounceTracking"] = "Anunciar qué logros están siendo rastreados para el grupo", --L["Announce Achievements Being Tracked To Group"]
+    ["GUI_OnlyTrackMissingAchievements"] = "Rastrea sólo los logros faltantes", --["Only track missing achievements"]
+    ["GUI_AnnounceMessagesToRaidWarning"] = "Anunciar mensajes como Aviso de Banda", --["Announce messages to Raid Warning"]
+    ["GUI_AchievementsDiscordTitle"] = "Discord de Logros",
+    ["GUI_AchievementsDiscordDescription"] = "Puedes contribuir amablemente con tácticas a través del Servidor de Discord, la comunidad donde los miembros pueden reunirse con coleccionistas de logros y formar grupos para una variedad de ellos.",
+    ["GUI_PlaySoundOnSuccess"] = "Reproduce un sonido cuando un logro se complete",
+    ["GUI_PlaySoundOnFailed"] = "Reproduce un sonido cuando un logro falle",
+    ["GUI_SelectSound"] = "Selecciona Sonido",
     ["GUI_TrackingDisabled"] = "Seguimiento de Logros Desactivado",
     ["GUI_EnterInstanceToStartScanning"] = "Entra en la mazmorra para comenzar a escanear",
     ["GUI_BattleForAzeroth"] = "Battle for Azeroth",
@@ -40,38 +40,38 @@ local baseLocale = {
     ["GUI_Track"] = "Seguir",
     ["GUI_OutputPlayers"] = "Informar a Jugadores",
     ["GUI_OutputTactics"] = "Informar Táctica",
-    -- ["GUI_OnlyDisplayMissingAchievements"] = "Only display missing achievements",
-    -- ["GUI_GreyOutCompletedAchievements"] = "Grey out completed achievements",
-    -- ["GUI_HideCompletedAchievements"] = "Hide completed achievements",
+    ["GUI_OnlyDisplayMissingAchievements"] = "Muestra sólo los logros que faltan",
+    ["GUI_GreyOutCompletedAchievements"] = "Ensombrece los logros completados",
+    ["GUI_HideCompletedAchievements"] = "Oculta los logros completados",
 
     -------------------------------------------------------
     --- Core
     -------------------------------------------------------    
-    -- ["Core_StartingAchievementScan"] = "Starting acchievement scan for", --["Starting Achievement Scan For"]
-    -- ["Core_GameFreezeWarning"] = "This may freeze your game for a few seconds", --["This may freeze your game for a few seconds"]
-    -- ["Core_AchievementScanFinished"] = "Achievement Scanning Finished", --["Achievement Scanning Finished"]
-    -- ["Core_EnableAchievementTracking"] = "Do you want to enable achievement tracking for", --["Do you want to enable achievement tracking for"]
-    -- ["Core_TrackAchievements"] = "Track Achievements", --["Track Achievements"]
-    -- ["Core_Yes"] = "Yes", --["Core_Yes"]
-    -- ["Core_No"] = "No", --["Core_No"]
-    -- ["Core_AchievementTrackingEnabledFor"] = "Achievement Tracking Enabled for", --["Achievement Tracking Enabled for"]
-    -- ["Core_help"] = "help", --["help"]
-    -- ["Core_Commands"] = "List of slash commands", --["List of slash commands"]
-    -- ["Core_ListCommands"] = "shows a list of avaliable slash commands", --["shows a list of avaliable slash commands"]
-    -- ["Core_CommandEnableTracking"] = "enable/disable IAT achievement tracking", --["enable/disable IAT achievement tracking"]
-    -- ["Core_Enable"] = "enable", --["enable"]
-    -- ["Core_NoTrackingForInstance"] = "IAT cannot track any achievements for this encounter.",
-    -- ["Core_Failed"] = "FAILED!",
-    -- ["Core_PersonalAchievement"] = "Personal Achievement",
-    -- ["Core_Reason"] = "Reason",
-    -- ["Core_CriteriaMet"] = "Criteria has been met. Boss can now be killed!",
-    -- ["Core_Counter"] = "Counter",
+    ["Core_StartingAchievementScan"] = "Comenzando exploración de logro para", --["Starting Achievement Scan For"]
+    ["Core_GameFreezeWarning"] = "Esto puede congelar el juego unos segundos", --["This may freeze your game for a few seconds"]
+    ["Core_AchievementScanFinished"] = "Escaneo de Logros Acabado", --["Achievement Scanning Finished"]
+    ["Core_EnableAchievementTracking"] = "¿Quieres activar el seguimiento de logros para", --["Do you want to enable achievement tracking for"]
+    ["Core_TrackAchievements"] = "Seguimiento de Logros", --["Track Achievements"]
+    ["Core_Yes"] = "Sí", --["Core_Yes"]
+    ["Core_No"] = "No", --["Core_No"]
+    ["Core_AchievementTrackingEnabledFor"] = "Escaneo de Logros Activado para", --["Achievement Tracking Enabled for"]
+    ["Core_help"] = "ayuda", --["help"]
+    ["Core_Commands"] = "Lista de comandos de texto", --["List of slash commands"]
+    ["Core_ListCommands"] = "muestra una lista de comandos de texto disponibles", --["shows a list of avaliable slash commands"]
+    ["Core_CommandEnableTracking"] = "activar/desactivar IAT escaneo de logros", --["enable/disable IAT achievement tracking"]
+    ["Core_Enable"] = "activar", --["enable"]
+    ["Core_NoTrackingForInstance"] = "IAT no puede rastrear ningún logro para este encuentro",
+    ["Core_Failed"] = "¡FALLADO!",
+    ["Core_PersonalAchievement"] = "Logro Personal",
+    ["Core_Reason"] = "Razón",
+    ["Core_CriteriaMet"] = "Requerimiento conseguido. ¡Puedes matar al Boss!",
+    ["Core_Counter"] = "Contrarresta",
 
     -------------------------------------------------------
     --- Instances
     -------------------------------------------------------  
-    -- ["Other"] = "Other", 
-    -- ["Instances_TrashAfterThirdBoss"] = "Trash after the third boss", --L["Trash after the third boss"]    
+    ["Other"] = "Otros", 
+    ["Instances_TrashAfterThirdBoss"] = "Pulls después del tercer boss", --L["Trash after the third boss"]    
 }
 
 if(GetLocale() == "esES") then
