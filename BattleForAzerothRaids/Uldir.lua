f90e4391431dc84240e5e8571f3164fda9a7e0b6
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-01-22 19:43:18 +0000

    Fixed tracking for Elevator Music

--- BattleForAzerothRaids/Uldir.lua
+++ BattleForAzerothRaids/Uldir.lua
@@ -43,7 +43,7 @@ local lastPlayerToAbsorbOrb = ""
 local achievementRedForAttempt = false
 
 function core._1861:Taloc()
-    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 280461 and orbCounter < 4 then
+    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 280461 and orbCounter < 4 then
         orbCounter = orbCounter + 1
         core:sendMessage(core:getAchievement() .. " " .. GetSpellLink(280461) .. " " .. L["Core_Counter"] .. " (" .. orbCounter .. "/4)")
     end
