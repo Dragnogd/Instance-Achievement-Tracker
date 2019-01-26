ec9c7a4bb5eb6e7d3335c04d09fe31387f2bf541
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-01-26 16:39:10 +0000

    Added debug messages for logging

--- BattleForAzerothRaids/BattleOfDazaralor.lua
+++ BattleForAzerothRaids/BattleOfDazaralor.lua
@@ -48,7 +48,12 @@ function core._2070:ChampionOfTheLight()
     --Player has gained Aspect of Jani debuff. They need to loose the debuff within 30 seconds with the tribute for counter to increment
     if core.type == "SPELL_AURA_APPLIED" and core.spellId == 288610 then
         --Set the player status to false since they don't have tribute yet
+        core:sendDebugMessage("Aspect of Jani Found")
+        if core.destName ~= nil then
+            core:sendDebugMessage(core.destName)
+        end
         if playersAspect1 == nil then
+            core:sendDebugMessage("Setting to player 1")
             playersAspect1 = core.destName
             playersAspect1Timer = C_Timer.NewTimer(30, function() 
                 playersAspect1Timer = nil
@@ -56,6 +61,7 @@ function core._2070:ChampionOfTheLight()
                 playersAspect1Tribute = nil
             end)
         elseif playersAspect2 == nil then
+            core:sendDebugMessage("Setting to player 2")
             playersAspect2 = core.destName
             playersAspect2Timer = C_Timer.NewTimer(30, function() 
                 playersAspect2Timer = nil
@@ -63,6 +69,7 @@ function core._2070:ChampionOfTheLight()
                 playersAspect2Tribute = nil
             end)
         elseif playersAspect3 == nil then
+            core:sendDebugMessage("Setting to player 3")
             playersAspect3 = core.destName
             playersAspect3Timer = C_Timer.NewTimer(30, function() 
                 playersAspect3Timer = nil
@@ -74,14 +81,22 @@ function core._2070:ChampionOfTheLight()
 
     --Player has stolen a shiny
     if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 288630 then
+        core:sendDebugMessage("Stolen Shiny")
+        if core.destName ~= nil then
+            core:sendDebugMessage(core.destName)
+            core:sendDebugMessage(core.destID)
+        end
         --Get mob which had stolen shiny from
         if core.destID == "145903" or core.destID == "147896" or core.destID == "145898" or core.destID == "147895" or core.destID == "144680" or core.destID == "144683" then
              --Set the stolen shiny to the player
             if core.sourceName == playersAspect1 then
+                core:sendDebugMessage("Found player 1")
                 playersAspect1Tribute = core.destID
             elseif core.sourceName == playersAspect2 then
+                core:sendDebugMessage("Found player 2")
                 playersAspect2Tribute = core.destID
             elseif core.sourceName == playersAspect3 then
+                core:sendDebugMessage("Found player 3")
                 playersAspect3Tribute = core.destID
             end
         end
@@ -90,11 +105,15 @@ function core._2070:ChampionOfTheLight()
     --Player has lost aspect
     --Check if this was because they handed it in or ran out of time
     if core.type == "SPELL_AURA_REMOVED" and core.spellId == 288610 then
-
+        core:sendDebugMessage("Aspect Lost")
+        if core.destName ~= nil then
+            core:sendDebugMessage(core.destName)
+        end
         --If player found then they handed shiny in on time
         local playerFound = false
         local shinyID = nil
         if core.destName == playersAspect1 then
+            core:sendDebugMessage("Player 1 Found")
             --Stop timer and increment counter
             if playersAspect1Timer ~= nil then
                 playersAspect1Timer:Cancel()
@@ -104,6 +123,7 @@ function core._2070:ChampionOfTheLight()
             playersAspect1Tribute = nil
             playerFound = true    
         elseif core.destName == playersAspect2 then
+            core:sendDebugMessage("Player 2 found")
             --Stop timer and increment counter
             if playersAspect2Timer ~= nil then
                 playersAspect2Timer:Cancel()
@@ -113,6 +133,7 @@ function core._2070:ChampionOfTheLight()
             playersAspect2Tribute = nil
             playerFound = true
         elseif core.destName == playersAspect3 then
+            core:sendDebugMessage("Player 3 found")
             --Stop timer and increment counter
             if playersAspect3Timer ~= nil then
                 playersAspect3Timer:Cancel()
@@ -124,13 +145,17 @@ function core._2070:ChampionOfTheLight()
         end
 
         if playerFound == true then   
+            core:sendDebugMessage("Found Player")
             if shinyID == "145903" or shinyID == "147896" then
+                core:sendDebugMessage("Crusader found")
                 crusadersCounter = crusadersCounter + 1
                 core:sendMessage(core:getAchievement() .. " " .. getNPCName(shinyID) .. " " .. L["Core_Counter"] .. "(" .. crusadersCounter .. "/3)") 
             elseif shinyID == "147895" or shinyID == "145898" then
+                core:sendDebugMessage("Disciple Found")
                 disciplesCounter = disciplesCounter + 1
                 core:sendMessage(core:getAchievement() .. " " .. getNPCName(shinyID) .. " " .. L["Core_Counter"] .. "(" .. disciplesCounter .. "/6)") 
             elseif shinyID == "144683" or shinyID == "144680" then
+                core:sendDebugMessage("Champion found")
                 championOfTheLightCounter = championOfTheLightCounter + 1
                 core:sendMessage(core:getAchievement() .. " " .. getNPCName(shinyID) .. " " .. L["Core_Counter"] .. "(" .. championOfTheLightCounter .. "/3)") 
             end
@@ -153,6 +178,7 @@ function core._2070:Grong()
     --Defeat Grong in the Battle of Dazar'alor after destroying 6 Barrels on Normal Difficulty or higher.
 
     if (core.type == "SPELL_AURA_APPLIED" or core.type == "SPELL_AURA_APPLIED_DOSE") and core.spellId == 289050 then
+        core:sendDebugMessage("Incrementing barrel counter")
         barrelCounter = barrelCounter + 1
         core:sendMessage(core:getAchievement() .. L["BattleOfDazzarlor_BarrelsDestroyed"] .. " (" .. barrelCounter .. "/6)")
     end
@@ -167,16 +193,19 @@ function core._2070:KingRastakhan()
 
     --Phantom of Retribution
     if (core.type == "UNIT_DIED" and core.destID == "146491") or (core.overkill > 0 and core.destID == "146491") then
+        core:sendDebugMessage("Retribtuion died")
         core:getAchievementFailed()
     end
 
     --Phantom of Rage
     if (core.type == "UNIT_DIED" and core.destID == "146492") or (core.overkill > 0 and core.destID == "146492") then
+        core:sendDebugMessage("Rage died")
         core:getAchievementFailed()
     end
 
     --Phantom of Slaughter
     if (core.type == "UNIT_DIED" and core.destID == "146493") or (core.overkill > 0 and core.destID == "146493") then
+        core:sendDebugMessage("Slaughter died")
         core:getAchievementFailed()
     end
 end
@@ -267,7 +296,8 @@ function core._2070.Events:CHAT_MSG_TEXT_EMOTE(self, message, sender, lineID, se
                             local _, _, _, _, _, _, _, _, _, spellId = UnitAura(sender, i)
                             if spellId == 284802 then
                                 --Check if the player actually needs the achievement since it is personal
-                                
+                                core:sendDebugMessage("Found player who hugged singing sunflower")
+                                core:sendDebugMessage(sender)
                                 InfoFrame_SetPlayerComplete(sender)
                                 playersCompletedAchievement = playersCompletedAchievement + 1
                                 core:getAchievementSuccessPersonalWithName(1, sender)	
@@ -283,7 +313,8 @@ function core._2070.Events:CHAT_MSG_TEXT_EMOTE(self, message, sender, lineID, se
                             local _, _, _, _, _, _, _, _, _, spellId = UnitAura(sender, i)
                             if spellId == 284802 then
                                 --Check if the player actually needs the achievement since it is personal
-                                
+                                core:sendDebugMessage("Found player who hugged singing sunflower")
+                                core:sendDebugMessage(sender)
                                 InfoFrame_SetPlayerComplete(sender)
                                 playersCompletedAchievement = playersCompletedAchievement + 1
                                 core:getAchievementSuccessPersonalWithName(1, sender)	
