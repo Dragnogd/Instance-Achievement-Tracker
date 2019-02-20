f8a7c45e9b0466ae3ea0861671dfd78a412f5662
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-02-20 18:47:39 +0000

    Added tracking for let it out and fixed tracking for Darker Side

--- LegionDungeons/SeatOfTheTriumvirate.lua
+++ LegionDungeons/SeatOfTheTriumvirate.lua
@@ -2,6 +2,7 @@
 -- Namespaces
 --------------------------------------
 local _, core = ...
+local L = core.L
 
 ------------------------------------------------------
 ---- Seat of the Triumvirate Bosses
@@ -12,10 +13,18 @@ core._1753 = {}
 ---- L'ura
 ------------------------------------------------------
 local crushingDarknessCounter = 0
+local crushingDarknessUID = {}
 local luraKilled = false
 
 function core._1753:ZuraalTheAscended()
     if core.type == "UNIT_DIED" and core.destID == "126283" then
+        core:getAchievementSuccess()
+    end
+end
+
+function core._1753:Saprish()
+    --Have Darkfang destroy 4 Sealed Void Caches using Shadow Pounce, then defeat Saprish in Seat of the Triumvirate on Mythic Difficulty.
+    if core:getBlizzardTrackingStatus(12005) == true then
         core:getAchievementSuccess()
     end
 end
@@ -31,16 +40,26 @@ function core._1753:Lura()
 end
 
 function core._1753:TrackAdditional()
-    --Gained Crushing Darkness
-    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 250821 then
-        crushingDarknessCounter = crushingDarknessCounter + 1
-        core:sendMessage(core.destName .. " Has gained Crushing Darkness (" .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
-    end
+    if core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].enabled == true then
+        --Gained Crushing Darkness
+        if core.type == "SPELL_AURA_APPLIED" and core.spellId == 250821 then
+            print("HERE 2")
+            if crushingDarknessUID[core.spawn_uid_dest_Player] == nil then
+                print("HERE 3")
+                crushingDarknessUID[core.spawn_uid_dest_Player] = core.spawn_uid_dest_Player
+                crushingDarknessCounter = crushingDarknessCounter + 1
+                core:sendMessage(core.destName .. " " .. L["Shared_HasGained"] .. " " .. GetSpellLink(250821) ..  " (" .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
+            end
+        end
 
-    --Lost Crushing Darkness
-    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 250821 and luraKilled == false then
-        crushingDarknessCounter = crushingDarknessCounter - 1
-        core:sendMessage(core.destName .. " Has lost Crushing Darkness ( " .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
+        --Lost Crushing Darkness
+        if core.type == "SPELL_AURA_REMOVED" and core.spellId == 250821 and luraKilled == false then
+            if crushingDarknessUID[core.spawn_uid_dest_Player] ~= nil then
+            crushingDarknessUID[core.spawn_uid_dest_Player] = nil
+                crushingDarknessCounter = crushingDarknessCounter - 1
+                core:sendMessage(core.destName .. " " .. L["Shared_HasLost"] .. " " .. GetSpellLink(250821) .. " (" .. crushingDarknessCounter .. "/" .. core.groupSize .. ")")
+            end 
+        end
     end
 end
 
