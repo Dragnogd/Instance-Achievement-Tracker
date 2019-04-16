7a80d02057daf2fad24d89c64f4346c35dddd66b
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-04-16 21:58:14 +0100

    Added tracking for Gotta Bounce

--- BattleForAzerothRaids/CrucibleOfStorms.lua
+++ BattleForAzerothRaids/CrucibleOfStorms.lua
@@ -8,4 +8,24 @@ local L = core.L
 ---- Battle of Dazar'alor Bosses
 ------------------------------------------------------
 core._2096 = {}
-core._2096.Events = CreateFrame("Frame")
+
+------------------------------------------------------
+---- The Restless Cabal
+------------------------------------------------------
+local voidCrashesActive = 0
+
+function core._2096:TheRestlessCabal()
+    --Defeat the Restless Cabal in Crucible of Storms after having at least 10 Void Crashes active simultaneously in Normal Difficulty or higher.
+
+    --Blizzard Tracker has gone white so achievement failed
+    if core:getBlizzardTrackingStatus(13501) == true then
+        core:getAchievementSuccess()
+    end
+end
+
+function core._2096:ClearVariables()
+    ------------------------------------------------------
+    ---- The Restless Cabal
+    ------------------------------------------------------
+    voidCrashesActive = 0
+end
