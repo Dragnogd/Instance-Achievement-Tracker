5862a3f4b38bb3627be37358f37d57e8d7aa717c
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-06-01 21:00:28 +0100

    Added tracking for You and What Army?

--- BattleForAzerothRaids/AzsharasEternalPalace.lua
+++ BattleForAzerothRaids/AzsharasEternalPalace.lua
@@ -10,10 +10,21 @@ local L = core.L
 core._2097 = {}
 core._2097.Events = CreateFrame("Frame")
 
+
+function core._2097:AbyssalCommanderSivara()
+    --Defeat Abyssal Commander Sivara in The Eternal Palace while all three of her lieutenants are alive and engaged in the fight on Normal difficulty or higher.
+
+    --Blizzard tracking gone red so fail achievement
+	if core:getBlizzardTrackingStatus(13684) == true then
+		core:getAchievementFailed()
+	end
+end
+
+
 function core._2097:Zaqul() 
     --Defeat Za'qul in the Eternal Palace after killing ten Twinklehoof Bovine on Normal difficulty or higher.
 
-    --Blizzard tracking gone white
+    --Blizzard tracking gone white so complete achievement
 	if core:getBlizzardTrackingStatus(13716) == true then
 		core:getAchievementSuccess()
 	end
