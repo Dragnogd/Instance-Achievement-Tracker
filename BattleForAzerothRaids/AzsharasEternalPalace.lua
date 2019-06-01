468eccfcbfe167f70a531de85ae6abbc1779fee4
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-06-01 20:59:20 +0100

    Added tracking for Lactose Intolerant

--- BattleForAzerothRaids/AzsharasEternalPalace.lua
+++ BattleForAzerothRaids/AzsharasEternalPalace.lua
@@ -9,3 +9,12 @@ local L = core.L
 ------------------------------------------------------
 core._2097 = {}
 core._2097.Events = CreateFrame("Frame")
+
+function core._2097:Zaqul() 
+    --Defeat Za'qul in the Eternal Palace after killing ten Twinklehoof Bovine on Normal difficulty or higher.
+
+    --Blizzard tracking gone white
+	if core:getBlizzardTrackingStatus(13716) == true then
+		core:getAchievementSuccess()
+	end
+end
