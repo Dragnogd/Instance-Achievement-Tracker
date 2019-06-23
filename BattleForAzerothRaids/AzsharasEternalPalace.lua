860a2792ed3d8d73536297e4fdbc8d37f0be2118
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-06-23 21:22:21 +0100

    Release V2.45.0

--- BattleForAzerothRaids/AzsharasEternalPalace.lua
+++ BattleForAzerothRaids/AzsharasEternalPalace.lua
@@ -10,7 +10,6 @@ local L = core.L
 core._2097 = {}
 core._2097.Events = CreateFrame("Frame")
 
-
 function core._2097:AbyssalCommanderSivara()
     --Defeat Abyssal Commander Sivara in The Eternal Palace while all three of her lieutenants are alive and engaged in the fight on Normal difficulty or higher.
 
@@ -19,7 +18,6 @@ function core._2097:AbyssalCommanderSivara()
 		core:getAchievementFailed()
 	end
 end
-
 
 function core._2097:Zaqul() 
     --Defeat Za'qul in the Eternal Palace after killing ten Twinklehoof Bovine on Normal difficulty or higher.
