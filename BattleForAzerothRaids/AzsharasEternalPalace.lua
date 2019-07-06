b1c68553ce60223644b48725ba0a54c7fd28ad44
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-07-06 12:06:25 +0100

    Added tracking for The Best of Us

--- BattleForAzerothRaids/AzsharasEternalPalace.lua
+++ BattleForAzerothRaids/AzsharasEternalPalace.lua
@@ -27,3 +27,42 @@ function core._2097:Zaqul()
 		core:getAchievementSuccess()
 	end
 end
+
+function core._2097:QueenAzshara()
+	--Defeat Queen Azshara in The Eternal Palace with one player still alive who is currently affected by Essence of Azeroth on Normal difficulty or higher.
+
+	if core.type == "UNIT_DIED" and core.currentDest = "Player" then
+		--Loop through all players in the group and check if just 1 player is alive
+		local playersAlive = 0
+		local lastPlayerAlive = nil
+        for i = 1, core.groupSize do
+            local unit = nil
+            if core.chatType == "PARTY" then
+                if i < core.groupSize then
+                    unit = "party" .. i
+                else
+                    unit = "player"
+                end
+            elseif core.chatType == "RAID" then
+                unit = "raid" .. i
+            elseif core.chatType == "SAY" then
+                unit = "player"
+            end
+        
+			if UnitIsDead(unit) == false then
+				playersAlive = playersAlive + 1
+				local name = UnitName(unit)
+				lastPlayerAlive = name
+			else
+		end
+		
+		if playersAlive == 1 then
+			for i=1,40 do
+                local _, _, _, _, _, _, _, _, _, spellId = UnitBuff(unit, i)
+				if spellId == 300866 then
+					core:getAchievementSuccess()
+                end
+            end
+		end
+	end
+end
