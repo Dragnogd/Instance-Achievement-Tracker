53b7301fb0db631ef4aac15e9a42026e593d5a20
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-07-10 21:04:02 +0100

    Altered tracking detection for Lactose Intolerant

--- BattleForAzerothRaids/AzsharasEternalPalace.lua
+++ BattleForAzerothRaids/AzsharasEternalPalace.lua
@@ -56,7 +56,7 @@ function core._2097:Zaqul()
     --Defeat Za'qul in the Eternal Palace after killing ten Twinklehoof Bovine on Normal difficulty or higher.
 
     --Blizzard tracking gone white so complete achievement
-	if core:getBlizzardTrackingStatus(13716) == true then
+	if core:getBlizzardTrackingStatus(13716, 1) == true then
 		core:getAchievementSuccess()
 	end
 end
