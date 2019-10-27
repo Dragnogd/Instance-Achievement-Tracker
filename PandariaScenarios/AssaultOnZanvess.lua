eb6946959a03f3071bc2b1cf1db88879b56a592a
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-10-27 14:50:53 +0000

    Added tracking for For The Swarm

--- PandariaScenarios/AssaultOnZanvess.lua
+++ PandariaScenarios/AssaultOnZanvess.lua
@@ -8,3 +8,10 @@ local L = core.L
 ---- Assault on Zan'vess
 ------------------------------------------------------
 core._1050 = {}
+
+function core._1050:CommanderTelvrak()
+    --Defeat Commander Tel'vrak without killing any Zan'thik Swarmers in the Assault on Zan'vess scenario.
+    if core.type == "UNIT_DIED" and core.destID == "67887" then
+        core:getAchievementFailed()
+    end
+end
