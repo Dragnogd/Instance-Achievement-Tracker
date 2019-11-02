dd8c05df92014d0bf66afe7f6fd6184aa220b5d0
Ryan Coombes <ryan.coombes1@ntlworld.com>
2019-11-02 12:32:32 +0000

    Added tracking for Monkey in the Middle and Spill No Evil

--- PandariaScenarios/UngaIngoo.lua
+++ PandariaScenarios/UngaIngoo.lua
@@ -8,3 +8,24 @@ local L = core.L
 ---- Unga Ingoo
 ------------------------------------------------------
 core._1048 = {}
+local spillNoEvilFailed = false
+
+function core._1048:CaptainOok()
+    if core.type == "SPELL_HEAL" and core.spellId == 121973 then
+        core:getAchievementFailed()
+    end
+end
+
+function core._1048:TrackAdditional()
+    --Brew the Boomer Brew without anyone in your party being hit by lightning during A Brewing Storm.
+    if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 123137 and spillNoEvilFailed == false then 
+        core:sendMessage(GetAchievementLink(7231) .. " " .. L["Core_Failed"],true,"failed")
+        spillNoEvilFailed = true
+    end
+end
+
+function core._1048:InstanceCleanup()
+    spillNoEvilFailed = false
+end
+
+
