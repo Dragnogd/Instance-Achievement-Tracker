0d9dc159e5a5c83068d93071214fd11af2d919f0
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-01-25 20:50:12 +0000

    Release V2.71.0

--- Debug.lua
+++ Debug.lua
@@ -7,5 +7,14 @@ local L = core.L
 
 function IATDEBUG()
     core:sendDebugMessage("-----Starting IAT DEBUGUGGER----")
+
+    --START Allowed Message functions to work
+    core.achievementIDs[1] = 14148
+    core.achievementsCompleted[1] = false
+    core.IATInfoFrame:ToggleOn()
+    core.IATInfoFrame:SetHeading(GetAchievementLink(14148))
+    core.achievementsFailed[1] = false
+    --END Allowed Message functions to work
+
     core:sendDebugMessage("-----FINISHED IAT DEBUGUGGER----")
 end
