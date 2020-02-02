d46596211eb84e02a1bed41105ddebcdea9072d6
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-02-02 20:22:20 +0000

    Release V2.72.0

--- Debug.lua
+++ Debug.lua
@@ -7,7 +7,6 @@ local L = core.L
 
 function IATDEBUG()
     core:sendDebugMessage("-----Starting IAT DEBUGUGGER----")
-
     --START Allowed Message functions to work
     core.achievementIDs[1] = 14148
     core.achievementsCompleted[1] = false
@@ -15,23 +14,4 @@ function IATDEBUG()
     core.IATInfoFrame:SetHeading(GetAchievementLink(14148))
     core.achievementsFailed[1] = false
     --END Allowed Message functions to work
-
-    core._2217:DarkInquisitorXanesh()
-
-    core.type = "SPELL_AURA_APPLIED"
-    core.spellId = 312406
-    core.destName = "Whizzey"
-
-    core._2217:DarkInquisitorXanesh()
-    core._2217:DarkInquisitorXanesh()
-    core._2217:DarkInquisitorXanesh()
-
-    C_Timer.After(3, function() 
-        core.type = "SPELL_AURA_REMOVED"
-        core._2217:DarkInquisitorXanesh()
-        core._2217:DarkInquisitorXanesh()
-        core._2217:DarkInquisitorXanesh()
-    end)
-
-    core:sendDebugMessage("-----FINISHED IAT DEBUGUGGER----")
 end
