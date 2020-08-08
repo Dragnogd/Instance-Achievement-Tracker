acabff814951ba5b556e59c868c78a7ca18612c2
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-08-08 21:01:44 +0100

    Test LibStub Location

--- libs/LibStub/LibStub.lua
+++ libs/LibStub/LibStub.lua
@@ -1,6 +1,6 @@
 -- $Id: LibStub.lua 76 2007-09-03 01:50:17Z mikk $
 -- LibStub is a simple versioning stub meant for use in Libraries.  http://www.wowace.com/wiki/LibStub for more info
--- LibStub is hereby placed in the Public Domain
+-- LibStub is hereby placed in the Public Domain 
 -- Credits: Kaelten, Cladhaire, ckknight, Mikk, Ammo, Nevcairiel, joshborke
 local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 2  -- NEVER MAKE THIS AN SVN REVISION! IT NEEDS TO BE USABLE IN ALL REPOS!
 local LibStub = _G[LIBSTUB_MAJOR]
