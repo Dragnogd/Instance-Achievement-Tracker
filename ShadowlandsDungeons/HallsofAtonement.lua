7b2ee67db136f3024caee37d44d026caf75a4d9d
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-08-19 21:40:03 +0100

    Added tracking for Picking Up the Pieces, Breaking Bad and Nobody Puts Denathrius in a Corner

--- ShadowlandsDungeons/HallsofAtonement.lua
+++ ShadowlandsDungeons/HallsofAtonement.lua
@@ -8,3 +8,27 @@ local L = core.L
 ---- Halls of Atonement
 ------------------------------------------------------
 core._2297 = {}
+
+function core._2297:Halkias()
+    --Defeat Halkias after he absorbs all three Shards of Halkas in the Halls of Atonement on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14567, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
+
+function core._2297:Echelon()
+    --Defeat Echelon after shatter 18 or more Undying Stonefiends at one time in the Halls of Atonement on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14284, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
+
+function core._2297:LordChamberlain()
+    --Defeat Lord Chamberlain with 3 Sinstone Statues in corners of the room in the Halls of Atonement on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14352, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
