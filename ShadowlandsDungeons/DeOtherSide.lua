581965f83fc19cbf54c21a71531b7e4da7436864
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-08-21 17:01:17 +0100

    Added tracking for Thinking With...

--- ShadowlandsDungeons/DeOtherSide.lua
+++ ShadowlandsDungeons/DeOtherSide.lua
@@ -8,3 +8,11 @@ local L = core.L
 ---- De Other Side
 ------------------------------------------------------
 core._2291 = {}
+
+function core._2291:DealerXyexa()
+    --Defeat Dealer Xy'exa after collecting all 5 orbs floating above her in De Other Side on Mythic Difficulty.
+
+    if core:getBlizzardTrackingStatus(14606, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
