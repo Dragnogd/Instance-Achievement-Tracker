6e505b28a8d6a497a3267e9b0349f7784ac1680e
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-08-21 18:22:28 +0100

    Added tracking for Residue Evil

--- ShadowlandsDungeons/SanguineDepths.lua
+++ ShadowlandsDungeons/SanguineDepths.lua
@@ -8,3 +8,11 @@ local L = core.L
 ---- Sanguine Depths
 ------------------------------------------------------
 core._2284 = {}
+
+function core._2284:ExecutorTarvold()
+    --Defeat Executor Tarvold after slaying 8 Forlorn Captives with the effects of Residue in Sanguine Depths on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14286, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
