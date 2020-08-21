f60bb7a5e8829b47e28ff0be667f45281b1269eb
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-08-21 17:04:21 +0100

    Added tracking for Hunger for Knowledge and Hooked on hydroponics

--- ShadowlandsDungeons/MistsofTirnaScithe.lua
+++ ShadowlandsDungeons/MistsofTirnaScithe.lua
@@ -8,3 +8,19 @@ local L = core.L
 ---- Mists of Tirna Scithe
 ------------------------------------------------------
 core._2290 = {}
+
+function core._2290:IngraMaloch()
+    --Defeat Ingra Maloch after defeating an Overgrown Hydra on in thie Mists of Tirna Scithe on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14503, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
+
+function core._2290:Tredova()
+    --Defeat Tred'ova after defeating an Elightened Spinemaw Gorger in the Mists of Tirna Scithe on Mythic difficulty.
+
+    if core:getBlizzardTrackingStatus(14375, 1) == true then
+        core:getAchievementSuccess()
+    end
+end
