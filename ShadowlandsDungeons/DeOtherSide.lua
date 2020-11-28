613613b8092b27c40c1bc9e2f0005c49bf97b20d
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-11-28 16:14:54 +0000

    Highly Communicable - InfoFrame displayed once on first boss, InfoFrame displays time left on debuff

--- ShadowlandsDungeons/DeOtherSide.lua
+++ ShadowlandsDungeons/DeOtherSide.lua
@@ -13,7 +13,8 @@ core._2291.Events = CreateFrame("Frame")
 ------------------------------------------------------
 ---- Highly Communicable
 ------------------------------------------------------
-local playersCompletedAchievement = 0
+local initialSetup = false
+local timerStarted = false
 
 ------------------------------------------------------
 ---- The Manastorms
@@ -52,11 +53,9 @@ function core._2291:TheManastorms()
     end
 end
 
-
 core._2291.Events:SetScript("OnEvent", function(self, event, ...)
     return self[event] and self[event](self, event, ...)
 end)
-
 
 function core._2291:InstanceCleanup()
     core._2291.Events:UnregisterEvent("UNIT_AURA")
@@ -65,16 +64,35 @@ end
 
 function core._2291:InitialSetup()
     core._2291.Events:RegisterEvent("UNIT_AURA")
-    C_Timer.After(5, function()
-        core.IATInfoFrame:ToggleOn()
-        InfoFrame_UpdatePlayersOnInfoFrame()
-        InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
-        core.IATInfoFrame:SetHeading(GetAchievementLink(14354))
-    end)
 end
 
 function core._2291:TrackAdditional()
-    InfoFrame_RefreshPlayersOnInfoFrame()
+    if initialSetup == true then
+        --InfoFrame_RefreshPlayersOnInfoFrameTable()
+
+        if timerStarted == false then
+            timerStarted = true
+            C_Timer.After(1, function()
+                if core.InfoFrame_PlayersTable ~= nil then
+                    for player,status in pairs(core.InfoFrame_PlayersTable) do
+                        for i=1,40 do
+                            local _, _, _, _, _, expirationTime, _, _, _, spellId = UnitDebuff(player, i)
+                            if spellId == 322746 then
+                                core.InfoFrame_PlayersTable[player] = {2, math.floor(expirationTime - GetTime())}
+                                InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+                            end
+                        end
+                    end
+
+                end
+                timerStarted = false
+
+                if core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].players ~= nil then
+                    core._2291:TrackAdditional()
+                end
+            end)
+        end
+    end
 end
 
 function core._2291.Events:UNIT_AURA(self, unitID)
@@ -87,28 +105,42 @@ function core._2291.Events:UNIT_AURA(self, unitID)
         end
     end
 
-    if foundCorruptedBlood == true then
-        InfoFrame_SetPlayerComplete(name)
-        InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasBeenMet"],"green")
+    if initialSetup == false and foundCorruptedBlood == true then
+        core.IATInfoFrame:SetHeading(GetAchievementLink(14354))
+        InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
+        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"])
+        core.IATInfoFrame:ToggleOn()
+        initialSetup = true
+        core._2291:TrackAdditional()
+    end
+
+    if foundCorruptedBlood == true and name ~= nil then
+        if InfoFrame_GetPlayerCompleteWithMessage(name) == false then
+            InfoFrame_SetPlayerCompleteWithMessage(name, "")
+            InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasBeenMet"],"green")
+        end
     else
-        InfoFrame_SetPlayerFailed(name)
+        InfoFrame_SetPlayerFailedWithMessage(name, "")
         if InfoFrame_IsAnyPlayerComplete() == false then
             InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
         end
     end
-    InfoFrame_UpdatePlayersOnInfoFrame()
+
+    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
 end
 
 function core._2291:ClearVariables()
-    ------------------------------------------------------
-    ---- Highly Communicable
-    ------------------------------------------------------
-    playersCompletedAchievement = 0
-
     ------------------------------------------------------
     ---- The Manastorms
     ------------------------------------------------------
     echoFingerLaserXtremeCompleted = false
     shadowfuryCompleted = false
     stepsCompleted = 0
+end
+
+function core._2291:InstanceCleanup()
+    ------------------------------------------------------
+    ---- Highly Communicable
+    ------------------------------------------------------
+    initialSetup = false
 end
