7223e68f1d9818bfcc1c5ecbecc4642660831372
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-11-29 18:41:32 +0000

    Highly Communicable - Rewrote Corrupted Blood detection. Delay fail for 0.5 seconds

--- ShadowlandsDungeons/DeOtherSide.lua
+++ ShadowlandsDungeons/DeOtherSide.lua
@@ -15,6 +15,7 @@ core._2291.Events = CreateFrame("Frame")
 ------------------------------------------------------
 local initialSetup = false
 local timerStarted = false
+local criteriaMet = false
 
 ------------------------------------------------------
 ---- The Manastorms
@@ -57,76 +58,86 @@ core._2291.Events:SetScript("OnEvent", function(self, event, ...)
     return self[event] and self[event](self, event, ...)
 end)
 
-function core._2291:InstanceCleanup()
-    core._2291.Events:UnregisterEvent("UNIT_AURA")
-    core.IATInfoFrame:ToggleOff()
-end
+function core._2291:HighlyCommunicable()
+    if initialSetup == true and criteriaMet == false then
+        core:getAchievementFailed()
+        core.achievementsCompleted[1] = false
+    end
 
-function core._2291:InitialSetup()
-    core._2291.Events:RegisterEvent("UNIT_AURA")
+    if initialSetup == true and criteriaMet == true then
+        core:getAchievementSuccess()
+        core.achievementsFailed[1] = false
+    end
 end
 
 function core._2291:TrackAdditional()
+
+    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 322746 and core.destName ~= nil then --322746
+        if UnitIsPlayer(core.destName) then
+            if initialSetup == false then
+                core:sendDebugMessage("Initial Setup for Highly Communicable")
+                core.IATInfoFrame:SetHeading(GetAchievementLink(14354))
+                InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
+                InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"])
+                core.IATInfoFrame:ToggleOn()
+                initialSetup = true
+                core.infoFrameShown = true
+            end
+
+            local name, realm = UnitName(core.destName)
+
+            if InfoFrame_GetPlayerCompleteWithMessage(name) == false then
+                InfoFrame_SetPlayerCompleteWithMessage(name, "")
+                InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasBeenMet"],"green")
+            end
+        end
+
+        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+    end
+
+    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 322746 and core.destName ~= nil then --322746
+        if UnitIsPlayer(core.destName) then
+            local name, realm = UnitName(core.destName)
+
+            InfoFrame_SetPlayerFailedWithMessage(name, "")
+            C_Timer.After(0.5, function()
+                if InfoFrame_IsAnyPlayerComplete() == false then
+                    InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
+                end
+            end)
+        end
+        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+    end
+
     if initialSetup == true then
         --InfoFrame_RefreshPlayersOnInfoFrameTable()
-
         if timerStarted == false then
             timerStarted = true
+            local updatePerformed = false
             C_Timer.After(1, function()
-                if core.InfoFrame_PlayersTable ~= nil then
-                    for player,status in pairs(core.InfoFrame_PlayersTable) do
-                        for i=1,40 do
-                            local _, _, _, _, _, expirationTime, _, _, _, spellId = UnitDebuff(player, i)
-                            if spellId == 322746 then
-                                core.InfoFrame_PlayersTable[player] = {2, math.floor(expirationTime - GetTime())}
-                                InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+                if InfoFrame_IsAnyPlayerComplete() == true then
+                    if core.InfoFrame_PlayersTable ~= nil then
+                        for player,status in pairs(core.InfoFrame_PlayersTable) do
+                            for i=1,40 do
+                                local _, _, _, _, _, expirationTime, _, _, _, spellId = UnitDebuff(player, i)
+                                if spellId == 322746 then ----322746
+                                    core.InfoFrame_PlayersTable[player] = {2, math.floor(expirationTime - GetTime())}
+                                    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+                                    updatePerformed = true
+                                end
                             end
                         end
-                    end
 
+                    end
                 end
                 timerStarted = false
 
-                if core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].players ~= nil then
+                if core.instance ~= nil and updatePerformed == true then
                     core._2291:TrackAdditional()
                 end
             end)
         end
     end
-end
-
-function core._2291.Events:UNIT_AURA(self, unitID)
-    local name, realm = UnitName(unitID)
-    local foundCorruptedBlood = false
-    for i=1,40 do
-        local _, _, count2, _, _, _, _, _, _, spellId = UnitDebuff(unitID, i)
-        if spellId == 322746 then
-            foundCorruptedBlood = true
-        end
-    end
-
-    if initialSetup == false and foundCorruptedBlood == true then
-        core.IATInfoFrame:SetHeading(GetAchievementLink(14354))
-        InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
-        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"])
-        core.IATInfoFrame:ToggleOn()
-        initialSetup = true
-        core._2291:TrackAdditional()
-    end
-
-    if foundCorruptedBlood == true and name ~= nil then
-        if InfoFrame_GetPlayerCompleteWithMessage(name) == false then
-            InfoFrame_SetPlayerCompleteWithMessage(name, "")
-            InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasBeenMet"],"green")
-        end
-    else
-        InfoFrame_SetPlayerFailedWithMessage(name, "")
-        if InfoFrame_IsAnyPlayerComplete() == false then
-            InfoFrame_SetHeaderWithColour(L["Shared_CriteriaHasNotBeenMet"],"red")
-        end
-    end
-
-    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
 end
 
 function core._2291:ClearVariables()
@@ -143,4 +154,5 @@ function core._2291:InstanceCleanup()
     ---- Highly Communicable
     ------------------------------------------------------
     initialSetup = false
+    criteriaMet = false
 end
