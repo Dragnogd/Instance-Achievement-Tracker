83a87829aa263ae03cc92eb2f795d0ef8e0e0d3f
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-11-29 18:30:26 +0000

    Kaal-ed-shot - Fixed tracking not working for players on different realms from person running IAT. - Announce fail if player looses Slammed!

--- ShadowlandsDungeons/SanguineDepths.lua
+++ ShadowlandsDungeons/SanguineDepths.lua
@@ -39,22 +39,40 @@ end
 
 function core._2284:GeneralKaal()
     --Defeat General Kaal while affected by Slammed! in the Sanguine Depths on Mythic difficulty.
-    --Player gained Slammed!
-    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 330055 then
-        if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, core.destName) == true then
-            if slammedUID[core.destName] == nil then
-                slammedCount = slammedCount + 1
-                slammedUID[core.destName] = core.destName
-                core:sendMessage(core.destName .. " " .. L["Shared_HasCompleted"] .. " " .. GetAchievementLink(14289) .. " (" .. L["Core_PersonalAchievement"] .. ") (" .. slammedCount .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ")",true)
-                InfoFrame_SetPlayerCompleteWithMessage(core.destName, GetSpellLink(330055))
-                InfoFrame_SetHeaderCounter(L["Shared_PlayersWhoNeedAchievement"],slammedCount,#core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players)
-                InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
+    if core.destName ~= nil then
+        local name, realm = UnitName(core.destName)
+
+        --Player gained Slammed!
+        if core.type == "SPELL_AURA_APPLIED" and core.spellId == 330055 then --330055
+            if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, name) == true then
+                if slammedUID[core.destName] == nil then
+                    slammedCount = slammedCount + 1
+                    slammedUID[core.destName] = core.destName
+                    core:sendMessage(core.destName .. " " .. L["Shared_HasCompleted"] .. " " .. GetAchievementLink(14289) .. " (" .. L["Core_PersonalAchievement"] .. ") (" .. slammedCount .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ")",true)
+                    InfoFrame_SetPlayerCompleteWithMessage(core.destName, GetSpellLink(330055))
+                    InfoFrame_SetHeaderCounter(L["Shared_PlayersWhoNeedAchievement"],slammedCount,#core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players)
+                    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
+                end
+            end
+        end
+
+        --Player lost Slammed!
+        if core.type == "SPELL_AURA_REMOVED" and core.spellId == 330055 then --330055
+            if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, name) == true then
+                if slammedUID[core.destName] ~= nil then
+                    slammedCount = slammedCount - 1
+                    slammedUID[core.destName] = nil
+                    core:sendMessage(name .. " " .. L["Shared_HasFailed"] .. " " .. GetAchievementLink(14289) .. " (" .. L["Core_PersonalAchievement"] .. ")",true,"failed")
+                    InfoFrame_SetPlayerFailedWithMessage(core.destName, "")
+                    InfoFrame_SetHeaderCounter(L["Shared_PlayersWhoNeedAchievement"],slammedCount,#core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players)
+                    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
+                end
             end
         end
     end
 
     --Everyone in the group has completed the achievement
-    if slammedCount == core.groupSize then
+    if slammedCount == #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players then
         core:getAchievementSuccess()
     end
 end
@@ -65,57 +83,61 @@ function core._2284:TrackAdditional()
         core:sendDebugMessage("General Kaal Killed")
     end
 
-    --Player gained Shadow Ball
-    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 330004 then
-        core:sendDebugMessage("Player has gained Shadow Ball")
+    if core.destName ~= nil then
+        local name, realm = UnitName(core.destName)
 
-        if initialSetup == false then
-            core:sendDebugMessage("Turning InfoFrame on")
-            core.IATInfoFrame:SetHeading(GetAchievementLink(14289))
-            InfoFrame_SetHeaderCounter(L["Shared_PlayersWhoNeedAchievement"],slammedCount,#core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players)
-            InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
-            core.IATInfoFrame:ToggleOn()
-            initialSetup = true
-        end
+        --Player gained Shadow Ball
+        if core.type == "SPELL_AURA_APPLIED" and core.spellId == 330004 then --330004
+            core:sendDebugMessage("Player has gained Shadow Ball")
 
-        if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, core.destName) == true then
-            if shadowBallUID[core.destName] == nil then
-                core:sendDebugMessage(core.destName)
-                shadowBallCount = shadowBallCount + 1
-                core:sendDebugMessage("Count: " .. shadowBallCount)
-                shadowBallUID[core.destName] = core.destName
-                core:sendMessage(core.destName .. " " .. L["Shared_HasGained"] .. " " .. GetSpellLink(330004) .. " (" .. shadowBallCount .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ")",true)
-                InfoFrame_SetPlayerNeutralWithMessage(core.destName, GetSpellLink(330004))
+            if initialSetup == false then
+                core:sendDebugMessage("Turning InfoFrame on")
+                core.IATInfoFrame:SetHeading(GetAchievementLink(14289))
+                InfoFrame_SetHeaderCounter(L["Shared_PlayersWhoNeedAchievement"],slammedCount,#core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players)
+                InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
+                core.IATInfoFrame:ToggleOn()
+                initialSetup = true
+                core.infoFrameShown = true
             end
+
+            if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, name) == true then
+                if shadowBallUID[core.destName] == nil then
+                    core:sendDebugMessage(core.destName)
+                    shadowBallCount = shadowBallCount + 1
+                    core:sendDebugMessage("Count: " .. shadowBallCount)
+                    shadowBallUID[core.destName] = core.destName
+                    core:sendMessage(core.destName .. " " .. L["Shared_HasGained"] .. " " .. GetSpellLink(330004) .. " (" .. shadowBallCount .. "/" .. #core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players .. ")",true)
+                    InfoFrame_SetPlayerNeutralWithMessage(core.destName, GetSpellLink(330004))
+                end
+            end
+
+            InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
         end
 
-        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
-        InfoFrame_RefreshPlayersOnInfoFrameWithAdditionalInfoPersonal(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"])
-    end
+        --Player looses Shadow Ball
+        if core.type == "SPELL_AURA_REMOVED" and core.spellId == 330004 then --330004
+            core:sendDebugMessage("Player has lost Shadow Ball")
+            if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, name) == true then
+                if shadowBallUID[core.destName] ~= nil then
+                    core:sendDebugMessage(core.destName)
+                    shadowBallCount = shadowBallCount - 1
+                    core:sendDebugMessage("Count: " .. shadowBallCount)
+                    shadowBallUID[core.destName] = nil
 
-    --Player looses Shadow Ball
-    if core.type == "SPELL_AURA_REMOVED" and core.spellId == 330004 then
-        core:sendDebugMessage("Player has lost Shadow Ball")
-        if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"].players, core.destName) == true then
-            if shadowBallUID[core.destName] ~= nil then
-                core:sendDebugMessage(core.destName)
-                shadowBallCount = shadowBallCount - 1
-                core:sendDebugMessage("Count: " .. shadowBallCount)
-                shadowBallUID[core.destName] = nil
-
-                local playerCheck = core.destName
-                core:sendDebugMessage("Checking if player has Slammed!")
-                --Check if player has SLAMMED! buff
-                C_Timer.After(10, function()
-                    --Player lost Shadow Ball but did not gain Slammed! so fail achievement
-                    core:sendDebugMessage("Checking " .. playerCheck)
-                    if slammedUID[playerCheck] == nil and generalKaalKilled == false then
-                        core:sendDebugMessage("No Slammed detected")
-                        core:sendMessage(playerCheck .. " " .. L["Shared_HasFailed"] .. " " .. GetAchievementLink(14289) .. " (" .. L["Core_PersonalAchievement"] .. ")",true,"failed")
-                        InfoFrame_SetPlayerFailedWithMessage(playerCheck, "")
-                        InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
-                    end
-                end)
+                    local playerCheck = core.destName
+                    core:sendDebugMessage("Checking if player has Slammed!")
+                    --Check if player has SLAMMED! buff
+                    C_Timer.After(10, function()
+                        --Player lost Shadow Ball but did not gain Slammed! so fail achievement
+                        core:sendDebugMessage("Checking " .. playerCheck)
+                        if slammedUID[playerCheck] == nil and generalKaalKilled == false then
+                            core:sendDebugMessage("No Slammed detected")
+                            core:sendMessage(playerCheck .. " " .. L["Shared_HasFailed"] .. " " .. GetAchievementLink(14289) .. " (" .. L["Core_PersonalAchievement"] .. ")",true,"failed")
+                            InfoFrame_SetPlayerFailedWithMessage(playerCheck, "")
+                            InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfoPersonalArgs(core.Instances[core.expansion][core.instanceType][core.instance]["boss3"])
+                        end
+                    end)
+                end
             end
         end
     end
