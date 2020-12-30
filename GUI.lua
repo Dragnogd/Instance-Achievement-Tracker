8e1f8a973390ee5fd3d99243760fe97a98ca1c21
Ryan Coombes <ryan.coombes1@ntlworld.com>
2020-12-30 20:32:08 +0000

    Release V3.13.1-Alpha

--- GUI.lua
+++ GUI.lua
@@ -41,7 +41,7 @@ AchievementTrackerNPCCache = {}
 -- Purpose:         Information about the current release. This is mianly used to detect which addon should output messages to chat to avoid spam
 Config.majorVersion = 3						--Addon with a higher major version change have priority over a lower major version
 Config.minorVersion = 13    				--Addon with a minor version change have prioirty over a lower minor version
-Config.revisionVersion = 0					--Addon with a revision change have the same priorty as a lower revision verison
+Config.revisionVersion = 1					--Addon with a revision change have the same priorty as a lower revision verison
 Config.releaseType = ""                     --Release type (Alpha, Beta, Release)
 
 -- Purpose:         Used to detect which version of the game the user is running. This is used so we can add features for different versions of the game.
