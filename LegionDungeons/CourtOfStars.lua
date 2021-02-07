65f348a6d05deab44f15488814c8db41b2a6f451
Ryan Coombes <ryan.coombes1@ntlworld.com>
2021-02-07 20:57:24 +0000

    Added tracking for Waiting For Gerdo

--- LegionDungeons/CourtOfStars.lua
+++ LegionDungeons/CourtOfStars.lua
@@ -8,5 +8,11 @@ local _, core = ...
 ------------------------------------------------------
 core._1571 = {}
 
+function core._1571:PatrolCaptainGerdo()
+    if core:getBlizzardTrackingStatus(10610) == true then
+		core:getAchievementSuccess()
+	end
+end
+
 function core._1571:ClearVariables()
 end
