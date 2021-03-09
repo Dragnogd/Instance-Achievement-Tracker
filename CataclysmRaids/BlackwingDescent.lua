29ab32df9be92bfe0142cfcea0a04222a031943c
Ryan Coombes <ryan.coombes1@ntlworld.com>
2021-03-09 20:35:28 +0000

    Keeping it in the Family - Fixed lua errors

--- CataclysmRaids/BlackwingDescent.lua
+++ CataclysmRaids/BlackwingDescent.lua
@@ -78,7 +78,7 @@ function core._669:OminitronDefenseSystem()
 			C_Timer.After(6, function()
 				--Cast takes about 5 seconds to complete
 				flameThrowerPlayer = nil
-				flameThrowerActive = false							
+				flameThrowerActive = false
 			end)
 		else
 			if core.destName ~= flameThrowerPlayer then
@@ -86,7 +86,7 @@ function core._669:OminitronDefenseSystem()
 				core:getAchievementFailedWithMessageAfter("(" .. L["Core_Reason"] .. ": " .. core.spellName)
 				flameThrowerFailed = true
 			end
-		end	
+		end
 	end
 end
 
@@ -107,57 +107,64 @@ function core._669:Chimaeron()
 end
 
 function core._669:Nefarian()
-	if f:IsEventRegistered("CHAT_MSG_MONSTER_YELL") == nil then
-		f:RegisterEvent("CHAT_MSG_MONSTER_YELL")
-	end
-	--Get percentage of onyxia and nefarian health
-	local onyxiaPercentage = nil
-	local nefarianPercentage = nil
-
-	--Find Nefarian
-	for i = 1, 4 do
-		if UnitName("boss" .. i) == "Nefarian" then
-			nefarianPercentage = (UnitHealth("boss" .. i) / UnitHealthMax("boss" .. i)) * 100
-		end 
+	if core:getBlizzardTrackingStatus(4849) == true then
+		core:getAchievementSuccess()
 	end
 
-	--Find Onyxia
-	if UnitHealth("boss1") ~= nil then
-		onyxiaPercentage = (UnitHealth("boss1") / UnitHealthMax("boss" .. i)) * 100
-	end
+	-- -- if f:IsEventRegistered("CHAT_MSG_MONSTER_YELL") == nil then
+	-- -- 	f:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	-- -- end
+	-- --Get percentage of onyxia and nefarian health
+	-- local onyxiaPercentage = nil
+	-- local nefarianPercentage = nil
 
-	if step1Announced == false then
-		core:sendMessage("Step 1: Damage Nefarian to below 65% but higher than 50%. Do not damge Onyxia")
-		step1Announced = true
-	end
 
-	if nefarianPercentage ~= nil and core.achievementsFailed[1] == false then
-		--Step 1: Damage Nefarian to atleast 60% but not lower than 50%
-		if nefarianPercentage > 50 and nefarianPercentage < 65 and onyxiaPercentage > 0 and step1Complete == false then
-			core:sendMessage("Step 1: Complete")
-			step1Complete = true
-			core:sendMessage("Step 2: Kill Onyxia")
-		elseif nefarianPercentage > 65 and onyxiaPercentage == 0 and step1Complete == false then
-			--If onyxia is killed before nefarian reaches step1 threshold then fail achievement
-			core:getAchievementFailedWithMessageAfter("Do not kill onyxia yet")	
-		end 
-		
-		--Step 2: Kill Onyxia
-		if step1Complete == true and onyxiaPercentage == 0 and nefarianPercentage > 50 and step2Complete == false then
-			core:sendMessage("Step 2: Complete")
-			step2Complete = true
-			core:sendMessage("Step 3: Damage Nefarian to below 50% once on platform but do not kill him")
-		elseif nefarianPercentage < 50 and step2Complete == false then
-			core:getAchievementFailedWithMessageAfter("do not damage Nefrian below 50%")			
-		end
+	-- for i = 1, 4 do
+	-- 	--Find Nefarian
+	-- 	if UnitGUID("boss" .. i) ~= nil then
+	-- 		local unitType, _, _, _, _, destID, spawn_uid_dest = strsplit("-", UnitGUID("boss" .. i))
+	-- 		if UnitName("boss" .. i) == "41376" then
+	-- 			nefarianPercentage = core:getHealthPercent("boss1")
+	-- 		end
+	-- 		--Find Onyxia
+	-- 		if UnitName("boss" .. i) == "41270" then
+	-- 			onyxiaPercentage = core:getHealthPercent("boss1")
+	-- 		end
+	-- 	end
+	-- end
 
-		--Step 3: Damage Nefarian to below 50% once on platforms
-		if step2Complete == true and nefarianPercentage < 50 and step3Complete == false then
-			core:sendMessage("Step 3: Complete")
-			step3Complete = true
-			core:sendMessage("Step 4: When Nefarian has landed kill him")			
-		end
-	end
+	-- if step1Announced == false then
+	-- 	core:sendMessage("Step 1: Damage Nefarian to below 65% but higher than 50%. Do not damge Onyxia",true)
+	-- 	step1Announced = true
+	-- end
+
+	-- if nefarianPercentage ~= nil and core.achievementsFailed[1] == false then
+	-- 	--Step 1: Damage Nefarian to atleast 60% but not lower than 50%
+	-- 	if nefarianPercentage > 50 and nefarianPercentage < 65 and onyxiaPercentage > 0 and step1Complete == false then
+	-- 		core:sendMessage("Step 1: Complete",true)
+	-- 		step1Complete = true
+	-- 		core:sendMessage("Step 2: Kill Onyxia",true)
+	-- 	elseif nefarianPercentage > 65 and onyxiaPercentage == 0 and step1Complete == false then
+	-- 		--If onyxia is killed before nefarian reaches step1 threshold then fail achievement
+	-- 		core:getAchievementFailedWithMessageAfter("Do not kill onyxia yet",true)
+	-- 	end
+
+	-- 	--Step 2: Kill Onyxia
+	-- 	if step1Complete == true and onyxiaPercentage == 0 and nefarianPercentage > 50 and step2Complete == false then
+	-- 		core:sendMessage("Step 2: Complete")
+	-- 		step2Complete = true
+	-- 		core:sendMessage("Step 3: Damage Nefarian to below 50% once on platform but do not kill him",true)
+	-- 	elseif nefarianPercentage < 50 and step2Complete == false then
+	-- 		core:getAchievementFailedWithMessageAfter("do not damage Nefrian below 50%",true)
+	-- 	end
+
+	-- 	--Step 3: Damage Nefarian to below 50% once on platforms
+	-- 	if step2Complete == true and nefarianPercentage < 50 and step3Complete == false then
+	-- 		core:sendMessage("Step 3: Complete",true)
+	-- 		step3Complete = true
+	-- 		core:sendMessage("Step 4: When Nefarian has landed kill him",true)
+	-- 	end
+	-- end
 end
 
 function core._669:ClearVariables()
@@ -208,19 +215,19 @@ core._669.Events:SetScript("OnEvent", function(self, event, ...)
     return self[event] and self[event](self, event, ...)
 end)
 
-function core._669.Events:CHAT_MSG_MONSTER_YELL(self, message, sender)
-    if core.Instances[core.expansion][core.instanceType][core.instance]["boss6"].enabled == true then
-		if step3Complete == true and step4Complete == false then
-			if string.find(message, "I have tried to be an accommodating host, but you simply will not die! Time to throw all pretense aside and just... KILL YOU ALL!") then
-				C_Timer.After(3, function()
-					core:sendMessage("Step 4: Complete")
-					core:getAchievementSuccess()							
-				end)
-				step4Complete = true
-			end
-		end
-    end
-end
+-- function core._669.Events:CHAT_MSG_MONSTER_YELL(self, message, sender)
+--     -- if core.Instances[core.expansion][core.instanceType][core.instance]["boss6"].enabled == true then
+-- 	-- 	if step3Complete == true and step4Complete == false then
+-- 	-- 		if string.find(message, "I have tried to be an accommodating host, but you simply will not die! Time to throw all pretense aside and just... KILL YOU ALL!") then
+-- 	-- 			C_Timer.After(3, function()
+-- 	-- 				core:sendMessage("Step 4: Complete")
+-- 	-- 				core:getAchievementSuccess()
+-- 	-- 			end)
+-- 	-- 			step4Complete = true
+-- 	-- 		end
+-- 	-- 	end
+--     -- end
+-- end
 
 function core._669.Events:UNIT_POWER_UPDATE(self, unit, powerType)
     if core.Instances[core.expansion][core.instanceType][core.instance]["boss4"].enabled == true then
