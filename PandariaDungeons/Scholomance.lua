6e1c2673887560ffe4e9b1e65588a8cf301705b2
Ryan Coombes <ryan.coombes1@ntlworld.com>
2021-03-13 17:59:22 +0000

    Fixed lua errors in scholomance

--- PandariaDungeons/Scholomance.lua
+++ PandariaDungeons/Scholomance.lua
@@ -2,26 +2,34 @@
 -- Namespaces
 --------------------------------------
 local _, core = ...
+local L = core.L
 
 ------------------------------------------------------
 ---- _1007 Bosses
 ------------------------------------------------------
 core._1007 = {}
 
+------------------------------------------------------
+---- Sanguinarian
+------------------------------------------------------
+local BoilingBloodThirstStacks = 0
+
 function core._1007:Rattlegore()
-    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.doseAmount > 5 and core.spellId == 113765 then
+    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount > 5 and core.spellId == 113765 then
         core:getAchievementFailed()
     end
 end
 
 function core._1007:Sanguinarian()
-    -- if core.type == "SPELL_AURA_APPLIED_DOSE" and core.spellId == 114141 then
-    --     print(core.doseAmount)
+    -- if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount <= 99 and core.spellId == 114141 then
+    --     if core.amount > BoilingBloodThirstStacks then
+    --         BoilingBloodThirstStacks = core.amount
+    --         core:sendMessage(GetAchievementLink(6396) .. " " .. GetSpellLink(114141) .. " " .. L["Core_Counter"] .. " (" .. BoilingBloodThirstStacks .. "/99)")
+    --     end
     -- end
-    
-    --TODO: need to test if this actually works
-    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.doseAmount == 99 and core.spellId == 114141 then
-        core:getAchievementFailed()
+
+    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount == 99 and core.spellId == 114141 then
+        core:getAchievementSuccess()
     end
 end
 
