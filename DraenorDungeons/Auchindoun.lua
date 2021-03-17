dbcaeb82a7ac71f96e35120fcb7b59a922d57b8d
Ryan Coombes <ryan.coombes1@ntlworld.com>
2021-03-17 18:56:02 +0000

    Added tracking for Demon's Souls

--- DraenorDungeons/Auchindoun.lua
+++ DraenorDungeons/Auchindoun.lua
@@ -14,5 +14,11 @@ function core._1182:SoulbinderNyami()
 	end
 end
 
+function core._1182:Azzakel()
+	if core:getBlizzardTrackingStatus(9551) == false then
+		core:getAchievementFailed()
+	end
+end
+
 function core._1182:ClearVariables()
 end
