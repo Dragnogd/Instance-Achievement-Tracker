ba89dc3ad9ed69fb8f64ceddb58c38a01b1d914f
Ryan Coombes <ryan.coombes1@ntlworld.com>
2021-06-28 21:22:56 +0100

    Cathedral of Eternal Night Updates

--- LegionDungeons/CathedralOfEternalNight.lua
+++ LegionDungeons/CathedralOfEternalNight.lua
@@ -2,6 +2,7 @@
 -- Namespaces
 --------------------------------------
 local _, core = ...
+local L = core.L
 
 ------------------------------------------------------
 ---- Cathedral of Eternal Night Bosses
@@ -11,50 +12,45 @@ core._1677 = {}
 ------------------------------------------------------
 ---- Thrashbite the Scornful
 ------------------------------------------------------
-local volumeCounter = 0
 local buff1Counter = 0
 local buff2Counter = 0
 local buff3Counter = 0
 local buff4Counter = 0
+local playerCounter = {}
 
 function core._1677:ThrashbiteTheScornful()
     --Volume 1: 121776 Voume 2: 121783 Volume 3: 121784 Voume 4: 121785
     --Buff 1: 243256 Buff 2: 243258 Buff 3: 243259 Buff 4: 243261
 
-    --Check if books spawn in correct order
-    if core.type == "SPELL_SUMMON" and core.spellId == 121776 and volumeCounter == 0 then
-        --Volume 1
-        volumeCounter = 1
-    elseif core.type == "SPELL_SUMMON" and core.spellId == 121783 and volumeCounter == 1 then
-        --Volume 2
-        volumeCounter = 2
-    elseif core.type == "SPELL_SUMMON" and core.spellId == 121784 and volumeCounter == 2 then
-        --Volume 3
-        volumeCounter = 3
-    elseif core.type == "SPELL_SUMMON" and core.spellId == 121785 and volumeCounter == 3 then
-        --Volume 3
-        volumeCounter = 4
-    elseif core.type == "SPELL_SUMMON" and (core.spellId == 121776 or core.spellId == 121783 or core.spellId == 121784 or core.spellId == 121785) then
-        --Books were read in the wrong order!!!
-        core:getAchievementFailedWithMessageAfter("Books read in the wrong order")
-    end
+    InfoFrame_UpdatePlayersOnInfoFrameWithAdditionalInfo()
+    InfoFrame_SetHeaderCounter(L["Shared_PlayersWithBuff"],buff4Counter,5)
 
     --Check if all players in group have read books
-    if core.type == "SPELL_AURA_APPLIED" and core.spellId == 243256 then
-        buff1Counter = buff1Counter + 1
-        core:sendMessage(core.destName .. " has read Volume 1 (" .. buff1Counter .. "/5)")
-    elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243258 then
-        buff2Counter = buff2Counter + 1
-        core:sendMessage(core.destName .. " has read Volume 2 (" .. buff2Counter .. "/5)")
-    elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243259 then
-        buff3Counter = buff3Counter + 1
-        core:sendMessage(core.destName .. " has read Volume 3 (" .. buff3Counter .. "/5)")
-    elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243261 then
-        buff4Counter = buff4Counter + 1
-        core:sendMessage(core.destName .. " has read Volume 4 (" .. buff4Counter .. "/5)")
+    if core.destName ~= nil then
+        if core.type == "SPELL_AURA_APPLIED" and core.spellId == 243256 then
+            buff1Counter = buff1Counter + 1
+            playerCounter[core.destName] = 1
+            core:sendMessage(core:getAchievement() .. " " .. core.destName .. " " .. L["Core_Counter"] .. " (" .. playerCounter[core.destName] .. "/5)",true)
+            InfoFrame_SetPlayerNeutralWithMessage(core.destName, playerCounter[core.destName])
+        elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243258 then
+            buff2Counter = buff2Counter + 1
+            playerCounter[core.destName] = playerCounter[core.destName] + 1
+            core:sendMessage(core:getAchievement() .. " " .. core.destName .. " " .. L["Core_Counter"] .. " (" .. playerCounter[core.destName] .. "/5)",true)
+            InfoFrame_SetPlayerNeutralWithMessage(core.destName, playerCounter[core.destName])
+        elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243259 then
+            buff3Counter = buff3Counter + 1
+            playerCounter[core.destName] = playerCounter[core.destName] + 1
+            core:sendMessage(core:getAchievement() .. " " .. core.destName .. " " .. L["Core_Counter"] .. " (" .. playerCounter[core.destName] .. "/5)",true)
+            InfoFrame_SetPlayerNeutralWithMessage(core.destName, playerCounter[core.destName])
+        elseif core.type == "SPELL_AURA_APPLIED" and core.spellId == 243261 then
+            buff4Counter = buff4Counter + 1
+            playerCounter[core.destName] = playerCounter[core.destName] + 1
+            core:sendMessage(core:getAchievement() .. " " .. core.destName .. " " .. L["Core_Counter"] .. " (" .. playerCounter[core.destName] .. "/5)",true)
+            InfoFrame_SetPlayerCompleteWithMessage(core.destName, playerCounter[core.destName])
 
-        if buff4Counter == 5 then
-            core:getAchievementSuccess()
+            if buff4Counter == 5 then
+                core:getAchievementSuccess()
+            end
         end
     end
 end
@@ -70,7 +66,6 @@ function core._1677:ClearVariables()
     ------------------------------------------------------
     ---- Thrashbite the Scornful
     ------------------------------------------------------
-    volumeCounter = 0
     buff1Counter = 0
     buff2Counter = 0
     buff3Counter = 0
