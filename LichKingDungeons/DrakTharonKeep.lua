3f369d8fa1ec15be2e9750950919a1b5f0089ab7
Ryan Coombes <ryan.coombes1@gmail.com>
2022-11-03 20:26:50 +0000

    Updated tracking for Drak'Tharon Keep

--- LichKingDungeons/DrakTharonKeep.lua
+++ LichKingDungeons/DrakTharonKeep.lua
@@ -2,6 +2,7 @@
 -- Namespaces
 --------------------------------------
 local _, core = ...
+local L = core.L
 
 ------------------------------------------------------
 ---- Drak'Tharon Keep Bosses
@@ -14,15 +15,31 @@ core._600 = {}
 local drakkariKilled = 0
 
 function core._600:Trollgore()
-    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount >= 10 then
+    --Defeat Trollgore in Drak'Tharon Keep on Heroic Difficulty before Consume reaches ten stacks.
+
+    if core.type == "SPELL_AURA_APPLIED_DOSE" and core.amount >= 10 and core.spellId == 59805 then
+        core:getAchievementFailed()
+    end
+end
+
+function core._600:NovosTheSummoner()
+    --Defeat Novos the Summoner in Drak'Tharon Keep on Heroic Difficulty without allowing any undead minions to reach the floor.
+
+    if core:getBlizzardTrackingStatus(2057) == false then
         core:getAchievementFailed()
     end
 end
 
 function core._600:KingDred()
+    --Engage King Dred in Drak'Tharon Keep on Heroic Difficulty and slay 6 Drakkari Gutrippers or Drakkari Scytheclaw during his defeat.
+
     if core.type == "UNIT_DIED" and (core.destID == "26641" or core.destID == "26628") and drakkariKilled < 6 then
         drakkariKilled = drakkariKilled + 1
-        core:sendMessage(core:getAchievement() .. " Drakkari Gutrippers/Scytheclaw Killed (" .. drakkariKilled .. "/6)")
+        if core.gameVersionMajor == 3 then
+            core:sendMessage(core:getAchievement() .. " " .. getNPCNameClassic(26641) .. "/" .. getNPCNameClassic(26628) .. " " .. L["Shared_Killed"] .. " (" .. drakkariKilled .. "/6)",true)
+        else
+            core:sendMessage(core:getAchievement() .. " " .. getNPCName(26641) .. "/" .. getNPCName(26628) .. " " .. L["Shared_Killed"] .. " (" .. drakkariKilled .. "/6)",true)
+        end
     end
 
     if drakkariKilled >= 6 then
