bc8af23595f915f493895db3f2de04a06abae5fb
Ryan Coombes <ryan.coombes1@gmail.com>
2023-01-15 14:38:12 +0000

    Finished implementing tracking + other fixes

--- DragonflightDungeons/TheAzureVault.lua
+++ DragonflightDungeons/TheAzureVault.lua
@@ -10,22 +10,26 @@ local L = core.L
 core._2515 = {}
 
 ------------------------------------------------------
----- The Azure Vault Bosses
+---- Other
+------------------------------------------------------
+local hidePlayersFailed = {}
+
+------------------------------------------------------
+---- Umbrelskul
 ------------------------------------------------------
 local geodeChunksKilled = 0
 
-function core._2515:Umbrelskul()
-    --Defeat Umbrelskul without being hit by a polymorph trap or triggering a Shriek in The Azure Vault on Mythic difficulty.
-
-    --TODO: Bruser says this is a group achievement
-end
+------------------------------------------------------
+---- Telash Greywing
+------------------------------------------------------
+local icyStacksCount = 0
 
 function core._2515:Umbrelskul2()
     --Defeat Umbrelskul after smashing Shimmering Geodes with Dragon Strike and destroying 9 Geode Chunks in The Azure Vault on Mythic difficulty.
 
     if core.type == "UNIT_DIED" and core.destID == "196696" then
         geodeChunksKilled = geodeChunksKilled + 1
-        core:sendMessage(core:getAchievement() .. " " .. getNPCName(196696) .. " " .. L["Shared_Killed"] .. " (" .. geodeChunksKilled .. "/3)",true)
+        core:sendMessage(core:getAchievement() .. " " .. getNPCName(196696) .. " " .. L["Shared_Killed"] .. " (" .. geodeChunksKilled .. "/9)",true)
     end
 
     if core:getBlizzardTrackingStatus(16331, 1) == true then
@@ -36,16 +40,56 @@ end
 function core._2515:TelashGreywing()
     --Defeat Telash Greywing after using Icy Devastator 12 times on an Icy Crystal to create a Very Icy Crystal in The Azure Vault on Mythic difficulty.
 
-    --TODO: Can we track the 12 stacks?
+    if (core.type == "SPELL_AURA_APPLIED" or core.type == "SPELL_AURA_APPLIED_DOSE") and core.destID == "197643" and core.spellId == 392417 then
+        icyStacksCount = icyStacksCount + 1
+        core:sendMessage(core:getAchievement() .. " " .. GetSpellLink(392417) .. " " .. L["Core_Counter"] .. " (" .. icyStacksCount .. "/12)",true)
+    end
 
     if core:getBlizzardTrackingStatus(16445, 1) == true then
 		core:getAchievementSuccess()
 	end
 end
 
+function core._2515:TrackAdditional()
+	--You Must Be Made of Hide
+	if core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].enabled == true then
+        --Polymorphed
+		if core.type == "SPELL_AURA_APPLIED" and core.spellId == 386368 then
+			if core.destName ~= nil then
+                local name, realm = UnitName(core.destName)
+                if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].players, name) == true and core:has_value(hidePlayersFailed, core.destName) == false then
+                    table.insert(hidePlayersFailed, core.destName)
+                    core:sendMessage(format(L["Shared_FailedPersonalAchievement"], core.destName, GetAchievementLink(16330), format(L["Shared_DamageFromAbility"], GetSpellLink(386368))),true)
+                end
+			end
+		end
+
+        --Shreiking Whelp
+        if core.type == "SPELL_CAST_SUCCESS" and core.spellId == 397726 then
+			if core.destName ~= nil then
+				local name, realm = UnitName(core.destName)
+                if core:has_value(core.Instances[core.expansion][core.instanceType][core.instance]["boss1"].players, name) == true and core:has_value(hidePlayersFailed, core.destName) == false then
+                    table.insert(hidePlayersFailed, core.destName)
+                    core:sendMessage(format(L["Shared_FailedPersonalAchievement"], core.destName, GetAchievementLink(16330), format(L["Shared_DamageFromAbility"], GetSpellLink(397726))),true)
+                end
+			end
+		end
+	end
+end
+
 function core._2515:ClearVariables()
+    ------------------------------------------------------
+    ---- Other
+    ------------------------------------------------------
+    hidePlayersFailed = {}
+
     ------------------------------------------------------
     ---- The Azure Vault Bosses
     ------------------------------------------------------
     geodeChunksKilled = 0
+
+    ------------------------------------------------------
+    ---- Telash Greywing
+    ------------------------------------------------------
+    icyStacksCount = 0
 end
