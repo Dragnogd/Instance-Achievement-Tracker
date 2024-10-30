491674e1953cd8dfafeb6046e2ef727184e3c6a8
Dragnogd <ryan.coombes1@ntlworld.com>
2024-10-30 17:02:16 +0000

    Increment version

--- GUI.lua
+++ GUI.lua
@@ -8,7 +8,7 @@ local Config = core.Config
 local IATInfoFrame = core.IATInfoFrame
 
 Config.majorVersion = 5
-Config.minorVersion = 12
+Config.minorVersion = 13
 Config.revisionVersion = 0
 Config.classicPhase = 5
 
