7d37cb51de2bf51d4fb5a1ccd0cbc19e6cfb93bc
Dragnogd <ryan.coombes1@ntlworld.com>
2025-08-10 21:47:42 +0100

    WIP

--- WarWithinRaids/ManaforgeOmega.lua
+++ WarWithinRaids/ManaforgeOmega.lua
@@ -3,3 +3,41 @@
 --------------------------------------
 local _, core = ...
 local L = core.L
+
+------------------------------------------------------
+---- Manaforge Omega
+------------------------------------------------------
+core._2810 = {}
+core._2810.Events = CreateFrame("Frame")
+
+function core._2810:PlexusSentinel()
+    -- Defeat the Plexus Sentinel after saving all mice from atomization in Manaforge Omega on Normal difficuty or higher.
+end
+
+function core._2810:Loomithar()
+    -- Defeat Loom'ithar after everyone decides whether they would rather /cower from it or /cuddle it before starting the battle in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:SoulbinderNaazindhri()
+    -- Defeat Soulbinder Naazindhri after defeating all Little Unbound Souls in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:ForgeweaverAraz()
+    -- Defeat Forgeweaver Araz after destroying a Void Forged Echo in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:SoulHunters()
+    -- Defeat the Soul Hunters after all players have worn Adarus' spare blindfold at least 1 time in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:Fractillus()
+    -- Defeat Fractillus after destroying a fourth wall 18 times in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:NexusKingSalhadaar()
+    -- Defeat Nexus-King Salhadaar alongside a hidden assassin in Manaforge Omega on Normal difficulty or higher.
+end
+
+function core._2810:DimensiusTheAllDevouring()
+    -- Defeat Dimensius, the All-Devouring after allowing every member of your raid to be hit by Reverse Gravity at least once in Manaforge Omega on Normal difficulty or higher.
+end
