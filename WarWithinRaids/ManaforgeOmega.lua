abdae8e50f34130f5b4a0ceed42fb8d98ad6b8bd
Dragnogd <ryan.coombes1@ntlworld.com>
2025-08-11 19:31:47 +0100

    Add blizzard tracking

--- WarWithinRaids/ManaforgeOmega.lua
+++ WarWithinRaids/ManaforgeOmega.lua
@@ -12,32 +12,64 @@ core._2810.Events = CreateFrame("Frame")
 
 function core._2810:PlexusSentinel()
     -- Defeat the Plexus Sentinel after saving all mice from atomization in Manaforge Omega on Normal difficuty or higher.
+
+    if core:getBlizzardTrackingStatus(42118, 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:Loomithar()
     -- Defeat Loom'ithar after everyone decides whether they would rather /cower from it or /cuddle it before starting the battle in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41613, 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:SoulbinderNaazindhri()
     -- Defeat Soulbinder Naazindhri after defeating all Little Unbound Souls in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41614, 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:ForgeweaverAraz()
     -- Defeat Forgeweaver Araz after destroying a Void Forged Echo in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41615, 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:SoulHunters()
     -- Defeat the Soul Hunters after all players have worn Adarus' spare blindfold at least 1 time in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41616, 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:Fractillus()
     -- Defeat Fractillus after destroying a fourth wall 18 times in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41617) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:NexusKingSalhadaar()
     -- Defeat Nexus-King Salhadaar alongside a hidden assassin in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41618 , 1) == true then
+        core:getAchievementSuccess()
+    end
 end
 
 function core._2810:DimensiusTheAllDevouring()
     -- Defeat Dimensius, the All-Devouring after allowing every member of your raid to be hit by Reverse Gravity at least once in Manaforge Omega on Normal difficulty or higher.
+
+    if core:getBlizzardTrackingStatus(41619 , 1) == true then
+        core:getAchievementSuccess()
+    end
 end
