a934fbf70d687112e998ad2ddc42eac9d6ef6238
Dragnogd <ryan.coombes1@ntlworld.com>
2025-09-10 21:22:31 +0100

    Release V5.38.0

--- GUI.lua
+++ GUI.lua
@@ -9,7 +9,7 @@ local Config = core.Config
 local IATInfoFrame = core.IATInfoFrame
 
 Config.majorVersion = 5
-Config.minorVersion = 37
+Config.minorVersion = 38
 Config.revisionVersion = 0
 Config.classicPhase = 7
 
