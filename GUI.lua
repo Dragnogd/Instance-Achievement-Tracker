81e5b5381c6ce1eb7093c27587376a7351875f77
Dragnogd <ryan.coombes1@ntlworld.com>
2025-12-04 20:16:17 +0000

    Bump toc for retail and mists

--- GUI.lua
+++ GUI.lua
@@ -9,7 +9,7 @@ local Config = core.Config
 local IATInfoFrame = core.IATInfoFrame
 
 Config.majorVersion = 5
-Config.minorVersion = 40
+Config.minorVersion = 41
 Config.revisionVersion = 0
 Config.classicPhase = 7
 
